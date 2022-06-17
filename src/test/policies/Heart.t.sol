8495a1fcfd761aba26da8108d42454f246f1d1f2
Oighty <90539730+Oighty@users.noreply.github.com>
2022-06-17 13:34:13 -0500

    Add policy to configure Price module (#26)

--- src/test/policies/Heart.t.sol
+++ src/test/policies/Heart.t.sol
@@ -5,18 +5,121 @@ import {Test} from "forge-std/Test.sol";
 import {UserFactory} from "test-utils/UserFactory.sol";
 import {console2} from "forge-std/console2.sol";
 import {Vm} from "forge-std/Vm.sol";
+import {Auth, Authority} from "solmate/auth/Auth.sol";
 
 import {MockERC20, ERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {MockPrice} from "../mocks/MockPrice.sol";
 import {MockAuthGiver} from "../mocks/MockAuthGiver.sol";
-import {MockOperator} from "../mocks/MockOperator.sol";
 
 import {FullMath} from "libraries/FullMath.sol";
 
-import {Kernel, Actions} from "../../Kernel.sol";
+import {Kernel, Policy, Actions} from "../../Kernel.sol";
 import {OlympusAuthority} from "modules/AUTHR.sol";
 
-import {Heart} from "../../policies/Heart.sol";
+import {Heart} from "policies/Heart.sol";
+
+import {IOperator, ERC20, IBondAuctioneer, IBondCallback} from "policies/interfaces/IOperator.sol";
+
+/**
+ * @notice Mock Operator to test Heart
+ */
+contract MockOperator is Policy, IOperator, Auth {
+    bool public result;
+    error Operator_CustomError();
+
+    constructor(Kernel kernel_)
+        Policy(kernel_)
+        Auth(address(kernel_), Authority(address(0)))
+    {
+        result = true;
+    }
+
+    /* ========== FRAMEWORK CONFIFURATION ========== */
+    function configureReads() external override onlyKernel {
+        setAuthority(Authority(getModuleAddress("AUTHR")));
+    }
+
+    function requestRoles()
+        external
+        view
+        override
+        onlyKernel
+        returns (Kernel.Role[] memory roles)
+    {}
+
+    /* ========== HEART FUNCTIONS ========== */
+    function operate() external requiresAuth {
+        if (!result) revert Operator_CustomError();
+    }
+
+    function setResult(bool result_) external {
+        result = result_;
+    }
+
+    /* ========== OPEN MARKET OPERATIONS (WALL) ========== */
+
+    function swap(ERC20 tokenIn_, uint256 amountIn_)
+        external
+        pure
+        returns (uint256 amountOut)
+    {
+        amountOut = 0;
+    }
+
+    function getAmountOut(ERC20 tokenIn_, uint256 amountIn_)
+        external
+        pure
+        returns (uint256)
+    {
+        return 0;
+    }
+
+    /* ========== OPERATOR CONFIGURATION ========== */
+    function setSpreads(uint256 cushionSpread_, uint256 wallSpread_) external {}
+
+    function setThresholdFactor(uint256 thresholdFactor_) external {}
+
+    function setCushionFactor(uint32 cushionFactor_) external override {}
+
+    function setCushionParams(
+        uint32 duration_,
+        uint32 debtBuffer_,
+        uint32 depositInterval_
+    ) external override {}
+
+    function setReserveFactor(uint32 reserveFactor_) external override {}
+
+    function setRegenParams(
+        uint32 wait_,
+        uint32 threshold_,
+        uint32 observe_
+    ) external override {}
+
+    function setBondContracts(
+        IBondAuctioneer auctioneer_,
+        IBondCallback callback_
+    ) external override {}
+
+    function initialize() external override {}
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    function fullCapacity(bool high_) external view override returns (uint256) {
+        return 0;
+    }
+
+    function status() external view override returns (Status memory) {
+        return
+            Status(
+                Regen(0, 0, 0, new bool[](0)),
+                Regen(0, 0, 0, new bool[](0))
+            );
+    }
+
+    function config() external view override returns (Config memory) {
+        return Config(0, 0, 0, 0, 0, 0, 0, 0);
+    }
+}
 
 contract HeartTest is Test {
     using FullMath for uint256;
