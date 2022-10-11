ceab0664d5039e35701a446c7f3f8b95ab67b43a
Oighty <oighty6@gmail.com>
2022-10-11 12:40:20 -0500

    script: deploy script updates to new contracts

---
+++ src/scripts/TreasuryCustodianDeploy.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.15;
+
+import {Script, console2} from "forge-std/Script.sol";
+
+import "src/Kernel.sol";
+import {TreasuryCustodian} from "policies/TreasuryCustodian.sol";
+import {RolesAdmin} from "policies/RolesAdmin.sol";
+
+/// @notice Script to deploy the Treasury Custodian Policy
+/// @dev    The address that this script is broadcast from must have write access to the contracts being configured
+contract TreasuryCustodianDeploy is Script {
+    Kernel public kernel;
+
+    /// Policies
+    TreasuryCustodian public treasuryCustodian;
+    RolesAdmin public rolesAdmin;
+
+    function deploy(address guardian_) external {
+        // Load addresses
+        kernel = Kernel(vm.envAddress("KERNEL"));
+        rolesAdmin = RolesAdmin(vm.envAddress("ROLESADMIN"));
+
+        vm.startBroadcast();
+
+        // Deploy treasury custodian
+
+        treasuryCustodian = new TreasuryCustodian(kernel);
+        console2.log("TreasuryCustodian deployed at:", address(treasuryCustodian));
+
+        /// Execute actions on Kernel
+        /// Approve policies
+        kernel.executeAction(Actions.ActivatePolicy, address(treasuryCustodian));
+
+        /// Configure access control for policies
+
+        // /// TreasuryCustodian roles
+        // rolesAdmin.grantRole("custodian", guardian_);
+
+        vm.stopBroadcast();
+    }
+}
