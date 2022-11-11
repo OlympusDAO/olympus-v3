57e1ab4709a7f94ed00966ebf4ffefd6a3df90ee
Oighty <oighty6@gmail.com>
2022-11-11 10:16:34 -0600

    chore: update foundry.toml with optimizer and fix shell script call

--- shell/verify_and_push_auth.sh
+++ shell/verify_and_push_auth.sh
@@ -2,5 +2,5 @@
 source .env
 
 # Verify and push auth using script
-forge script ./src/scripts/Deploy.sol:OlympusDeploy --sig "verifyAndPushAuth(address,address)()" $GUARDIAN_ADDRESS $POLICY_ADDRESS $EMERGENCY_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvv \
-# --broadcast
+forge script ./src/scripts/Deploy.sol:OlympusDeploy --sig "verifyAndPushAuth(address,address,address)()" $GUARDIAN_ADDRESS $POLICY_ADDRESS $EMERGENCY_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvv \
+--broadcast
