bb4bbdf6060f18ba1226cd378c271d7d8cd17098
Oighty <oighty6@gmail.com>
2022-11-28 15:24:53 -0600

    feat: make operator variable on Heart public + add setter

--- src/test/policies/Heart.t.sol
+++ src/test/policies/Heart.t.sol
@@ -233,6 +233,7 @@ contract HeartTest is Test {
     /// DONE
     /// [X] resetBeat
     /// [X] activate and deactivate
+    /// [X] setOperator
     /// [X] setRewardTokenAndAmount
     /// [X] withdrawUnspentRewards
     /// [X] cannot call admin functions without permissions
@@ -279,6 +280,15 @@ contract HeartTest is Test {
         /// Expect the heart to be active again and lastBeat to be reset
         assertTrue(heart.active());
         assertEq(heart.lastBeat(), block.timestamp - heart.frequency());
+    }
+
+    function testCorrectnes_setOperator(address newOperator) public {
+        /// Set the operator using the provided address
+        vm.prank(policy);
+        heart.setOperator(newOperator);
+
+        /// Check that the operator has been updated
+        assertEq(address(heart.operator()), newOperator);
     }
 
     function testCorrectness_setRewardTokenAndAmount() public {
