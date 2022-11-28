9e82907ff8db8ab5faabae7dc0f35a5caf6132be
Oighty <oighty6@gmail.com>
2022-11-28 16:43:28 -0600

    scripts: remove extra shell scripts

--- shell/deploy_custodian.sh
+++
@@ -1,7 +0,0 @@
-# Load environment variables
-source .env
-
-# Deploy using script
-forge script ./src/scripts/TreasuryCustodianDeploy.sol:TreasuryCustodianDeploy --sig "deploy(address)()" $GUARDIAN_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvv \
---broadcast --verify --etherscan-api-key $ETHERSCAN_KEY #\ # uncomment to broadcast to the network
-# --resume # uncomment to resume from a previous deployment
--- shell/deploy_distributor.sh
+++
@@ -1,7 +0,0 @@
-# Load environment variables
-source .env
-
-# Deploy using script
-forge script ./src/scripts/DistributorDeploy.sol:DistributorDeploy --sig "deploy(address)()" $POLICY_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvv \
---broadcast --verify --etherscan-api-key $ETHERSCAN_KEY #\ # uncomment to broadcast to the network
-# --resume # uncomment to resume from a previous deployment
