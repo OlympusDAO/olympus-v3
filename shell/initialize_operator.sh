02cc40b7677f3ba04b6f64b9c20aa6eb2961bbb8
indigo <indigo.eth@protonmail.com>
2023-02-01 18:29:48 -0600

    chore: update forge-std, bring in pigeon deps

--- shell/initialize_operator.sh
+++ shell/initialize_operator.sh
@@ -2,6 +2,6 @@
 source .env
 
 # Deploy using script
-forge script ./src/scripts/OperatorDeploy.sol:OperatorDeploy --sig "initialize()()" --rpc-url $RPC_URL --private-key $GUARDIAN_PRIVATE_KEY --slow -vvv \
+forge script ./src/scripts/OperatorDeploy.sol:OperatorDeploy --sig "initializeOperator()()" --rpc-url $RPC_URL --private-key $GUARDIAN_PRIVATE_KEY --slow -vvv \
 # --broadcast \ # uncomment to broadcast to the network
 # --resume # uncomment to resume from a previous call
