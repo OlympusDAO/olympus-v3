9a909cc3f16f319915500a244f0984522764bd56
Oighty <oighty6@gmail.com>
2023-02-15 13:28:33 -0600

    chore: fix deploy conflicts with forge-std

--- shell/initialize.sh
+++ shell/initialize.sh
@@ -2,6 +2,6 @@
 source .env
 
 # Deploy using script
-forge script ./src/scripts/Deploy.sol:OlympusDeploy --sig "initialize()()" --rpc-url $RPC_URL --private-key $GUARDIAN_PRIVATE_KEY --froms $GUARDIAN_ADDRESS --slow -vvv \
+forge script ./src/scripts/Deploy.sol:OlympusDeploy --sig "initializeOperator()()" --rpc-url $RPC_URL --private-key $GUARDIAN_PRIVATE_KEY --froms $GUARDIAN_ADDRESS --slow -vvv \
 --broadcast \ # uncomment to broadcast to the network
 # --resume # uncomment to resume from a previous call
