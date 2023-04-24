80de25b69e62a5e13020a0dd86b10a12333bda37
kanema20 <kanema20@gmail.com>
2023-04-24 16:13:46 -0400

    typechain deleted

--- src/test/Kernel.t.sol
+++ src/test/Kernel.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 
 import {Test} from "forge-std/Test.sol";
 import {UserFactory} from "test/lib/UserFactory.sol";
