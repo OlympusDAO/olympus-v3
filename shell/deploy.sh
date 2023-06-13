c7c53ee4221168684d0010ad714d992aa4933964
Jem <0x0xjem@gmail.com>
2023-06-13 17:58:25 +0400

    Disable broadcast by default

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -3,5 +3,5 @@ source .env
 
 # Deploy using script
 forge script ./src/scripts/DeployV2.sol:OlympusDeploy --sig "deploy(string,address,address,address)()" $CHAIN $GUARDIAN_ADDRESS $POLICY_ADDRESS $EMERGENCY_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY --slow -vvv \
---broadcast --verify --etherscan-api-key $ETHERSCAN_KEY #\ # uncomment to broadcast to the network
+# --broadcast --verify --etherscan-api-key $ETHERSCAN_KEY #\ # uncomment to broadcast to the network
 # --resume # uncomment to resume from a previous deployment
