1412fa637f005fcd6c8d8969b83d550aace3653b
Jem <0x0xjem@gmail.com>
2023-10-18 16:30:42 +0400

    Add decimal handling and errors to the ERC4626 submodule

--- src/modules/PRICE/submodules/feeds/ERC4626Price.sol
+++ src/modules/PRICE/submodules/feeds/ERC4626Price.sol
@@ -2,11 +2,42 @@
 pragma solidity 0.8.15;
 
 import "modules/PRICE/PRICE.v2.sol";
+import {FullMath} from "src/libraries/FullMath.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
 
 contract ERC4626Price is PriceSubmodule {
+    using FullMath for uint256;
+
+    /// @notice     Any token or pool with a decimal scale greater than this would result in an overflow
+    uint8 internal constant BASE_10_MAX_EXPONENT = 50;
+
+    // TODO
+    // [X] handle different decimals between ERC4626 and underlying
+    // [ ] assert asset decimals within bounds
+    // [ ] assert underlying decimals within bounds
+    // [ ] assert underlying is set
+
     // ========== ERRORS ========== //
+
+    /// @notice                     The value for output decimals is more than the maximum decimals allowed
+    /// @param outputDecimals_      The output decimals provided as a parameter
+    /// @param maxDecimals_         The maximum decimals allowed
+    error ERC4626_OutputDecimalsOutOfBounds(uint8 outputDecimals_, uint8 maxDecimals_);
+
+    /// @notice                     The value for the ERC4626 decimals is more than the maximum decimals allowed
+    /// @param assetDecimals_       The asset decimals
+    /// @param maxDecimals_         The maximum decimals allowed
+    error ERC4626_AssetDecimalsOutOfBounds(uint8 assetDecimals_, uint8 maxDecimals_);
+
+    /// @notice                     The value for the ERC4626 underlying asset's decimals is more than the maximum decimals allowed
+    /// @param underlyingDecimals_  The underlying asset decimals
+    /// @param maxDecimals_         The maximum decimals allowed
+    error ERC4626_UnderlyingDecimalsOutOfBounds(uint8 underlyingDecimals_, uint8 maxDecimals_);
+
+    /// @notice                     The underlying asset is not set
+    /// @param asset_               The address of the ERC4626 asset
+    error ERC4626_UnderlyingNotSet(address asset_);
 
     // ========== EVENTS ========== //
 
@@ -31,6 +62,8 @@ contract ERC4626Price is PriceSubmodule {
         uint8 outputDecimals_,
         bytes calldata params_
     ) external view returns (uint256) {
+        uint256 outputScale = 10 ** outputDecimals_;
+
         // We assume that the asset passed conforms to ERC4626
         ERC4626 asset = ERC4626(asset_);
         uint256 assetScale = 10 ** asset.decimals();
@@ -38,17 +71,21 @@ contract ERC4626Price is PriceSubmodule {
         // Get the underlying asset from the ERC4626
         address underlying = address(asset.asset());
 
+        // Get the decimals for the underlying asset
+        uint256 underlyingScale = 10 ** ERC20(underlying).decimals();
+
         // Get the number of underlying tokens per share
-        // We assume that the underlying and asset have the same decimals
-        uint256 underlyingPerShare = asset.convertToAssets(assetScale);
+        // Scale: output decimals
+        uint256 underlyingPerShare = asset.convertToAssets(assetScale).mulDiv(outputScale, underlyingScale);
 
         // Get the price of the underlying asset
+        // We assume that getPrice() returns in outputDecimals
         uint256 underlyingPrice = _PRICE().getPrice(underlying);
 
         // Calculate the price of the asset
-        uint256 assetPrice = (underlyingPrice * underlyingPerShare) / assetScale;
+        // Scale: output decimals
+        uint256 assetPrice = underlyingPrice.mulDiv(underlyingPerShare, outputScale);
 
-        // Convert to output decimals and return
-        return (assetPrice * 10 ** outputDecimals_) / assetScale;
+        return assetPrice;
     }
 }
