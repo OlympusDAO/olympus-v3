cbc91e6bc2a60ce069acd755e942f73100248a93
0xrusowsky <0xrusowsky@proton.me>
2023-11-03 18:50:45 +0100

    test: TRSRYv1.1 asset management

--- src/test/modules/TRSRY.v1_1.t.sol
+++ src/test/modules/TRSRY.v1_1.t.sol
@@ -11,7 +11,7 @@ import {OlympusERC20Token} from "src/external/OlympusERC20.sol";
 //import {MockPolicy} from "test/mocks/KernelTestMocks.sol";
 
 import {OlympusTreasury} from "src/modules/TRSRY/OlympusTreasury.sol";
-import {TRSRYv1_1} from "src/modules/TRSRY/TRSRY.v1.sol";
+import {TRSRYv1_1, Category, CategoryGroup} from "src/modules/TRSRY/TRSRY.v1.sol";
 
 import "src/Kernel.sol";
 
@@ -60,26 +60,26 @@ import "src/Kernel.sol";
 //      [ ] reverts if category does not exist
 //
 // Data Management
-// [ ] addAsset - adds an asset configuration to the treasury
-//      [ ] reverts if asset is already configured
-//      [ ] asset data stored correctly
-//      [ ] zero locations
-//      [ ] one location
-//      [ ] many locations
-// [ ] addAssetLocation - adds a location to an asset configuration
-//      [ ] reverts if asset is not configured
-//      [ ] reverts if location is already configured
-//      [ ] location data stored correctly
-//      [ ] zero locations prior
-//      [ ] one location prior
-//      [ ] many locations prior
-// [ ] removeAssetLocation - removes a location from an asset configuration
-//      [ ] reverts if asset is not configured
-//      [ ] reverts if location is not configured
-//      [ ] location data removed correctly
-//      [ ] zero locations after
-//      [ ] one location after
-//      [ ] many locations after
+// [X] addAsset - adds an asset configuration to the treasury
+//      [X] reverts if asset is already configured
+//      [X] reverts if asset is not a contract
+//      [X] asset data stored correctly
+//      [X] zero locations prior
+//      [X] one location prior
+//      [X] many locations prior
+// [X] addAssetLocation - adds a location to an asset configuration
+//      [X] reverts if asset is not configured
+//      [X] reverts if location is already configured
+//      [X] location data stored correctly
+//      [X] zero locations prior
+//      [X] one location prior
+//      [X] many locations prior
+// [X] removeAssetLocation - removes a location from an asset configuration
+//      [X] reverts if asset is not configured
+//      [X] location data removed correctly
+//      [X] zero locations after
+//      [X] one location after
+//      [X] many locations after
 // [ ] addCategoryGroup - adds an asset category group to the treasury
 //      [ ] reverts if category group is already configured
 //      [ ] category group data stored correctly
@@ -102,4 +102,275 @@ contract TRSRYv1_1Test is Test {
 
     Kernel internal kernel;
     OlympusTreasury public TRSRY;
+    MockERC20 public reserve;
+    address public godmode;
+
+    uint256 internal constant INITIAL_TOKEN_AMOUNT = 100e18;
+
+    function setUp() public {
+        kernel = new Kernel();
+        TRSRY = new OlympusTreasury(kernel);
+        reserve = new MockERC20("Reserve", "RSRV", 18);
+
+        kernel.executeAction(Actions.InstallModule, address(TRSRY));
+
+        // Generate test fixture policy addresses with different authorizations
+        godmode = TRSRY.generateGodmodeFixture(type(OlympusTreasury).name);
+        kernel.executeAction(Actions.ActivatePolicy, godmode);
+
+        // Give TRSRY some tokens
+        reserve.mint(address(TRSRY), INITIAL_TOKEN_AMOUNT);
+    }
+
+    // =================== ASSET DATA MANAGEMENT ===================== //
+
+    // -- Test: addAsset -------------------------------
+
+    function test_addAsset() public {
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.approved, true);
+        assertEq(asset.updatedAt, block.timestamp);
+        assertEq(asset.lastBalance, INITIAL_TOKEN_AMOUNT);
+        assertEq(asset.locations.length, 0);
+
+        // Verify asset list
+        address[] memory assets = TRSRY.getAssets();
+        assertEq(assets.length, 1);
+        assertEq(assets[0], address(reserve));
+    }
+
+    function test_addAsset_onePreviousLocation() public {
+        reserve.mint(address(1), INITIAL_TOKEN_AMOUNT);
+
+        address[] memory locations = new address[](1);
+        locations[0] = address(1);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.approved, true);
+        assertEq(asset.updatedAt, block.timestamp);
+        assertEq(asset.lastBalance, 2 * INITIAL_TOKEN_AMOUNT);
+        assertEq(asset.locations.length, 1);
+
+        // Verify asset list
+        address[] memory assets = TRSRY.getAssets();
+        assertEq(assets.length, 1);
+        assertEq(assets[0], address(reserve));
+    }
+
+    function test_addAsset_manyPreviousLocations() public {
+        reserve.mint(address(1), INITIAL_TOKEN_AMOUNT);
+        reserve.mint(address(2), INITIAL_TOKEN_AMOUNT);
+
+        address[] memory locations = new address[](2);
+        locations[0] = address(1);
+        locations[1] = address(2);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.approved, true);
+        assertEq(asset.updatedAt, block.timestamp);
+        assertEq(asset.lastBalance, 3 * INITIAL_TOKEN_AMOUNT);
+        assertEq(asset.locations.length, 2);
+
+        // Verify asset list
+        address[] memory assets = TRSRY.getAssets();
+        assertEq(assets.length, 1);
+        assertEq(assets[0], address(reserve));
+    }
+
+    function testRevert_addAsset_AssetAlreadyApproved() public {
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+
+        /// Try to add an already approved asset
+        bytes memory err = abi.encodeWithSignature("TRSRY_AssetAlreadyApproved(address)", reserve);
+        vm.expectRevert(err);
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+    }
+
+    function testRevert_addAsset_AssetNotContract(address notAsset_) public {
+        /// Try to add an address which is not a contract
+        bytes memory err = abi.encodeWithSignature("TRSRY_AssetNotContract(address)", notAsset_);
+        vm.expectRevert(err);
+        vm.prank(godmode);
+        TRSRY.addAsset(notAsset_, new address[](0));
+    }
+
+    // -- Test: addAssetLocation -------------------------------
+
+    function testFuzz_addAssetLocation(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.approved, true);
+        assertEq(asset.updatedAt, block.timestamp);
+        assertEq(asset.lastBalance, INITIAL_TOKEN_AMOUNT);
+        assertEq(asset.locations.length, 1);
+        assertEq(asset.locations[0], allocator_);
+    }
+
+    function testFuzz_addAssetLocation_onePreviousLocation(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+        vm.assume(allocator_ != address(1));
+
+        address[] memory locations = new address[](1);
+        locations[0] = address(1);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Verify asset list
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.locations.length, 2);
+        assertEq(asset.locations[1], allocator_);
+    }
+
+    function testFuzz_addAssetLocation_manyPreviousLocations(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+        vm.assume(allocator_ != address(1));
+        vm.assume(allocator_ != address(2));
+
+        address[] memory locations = new address[](2);
+        locations[0] = address(1);
+        locations[1] = address(2);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Verify asset list
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.locations.length, 3);
+        assertEq(asset.locations[2], allocator_);
+    }
+
+    function testRevert_addAssetLocation_AssetNotApproved() public {
+        /// Try to add the location of an asset which is not approved
+        bytes memory err = abi.encodeWithSignature("TRSRY_AssetNotApproved(address)", reserve);
+        vm.expectRevert(err);
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), address(1));
+    }
+
+    function testRevert_addAssetLocation_AddresZero() public {
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+
+        /// Try to add address(0) as the location
+        vm.expectRevert();
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), address(0));
+    }
+
+    // -- Test: removeAssetLocation -------------------------------
+
+    function testFuzz_removeAssetLocation(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), new address[](0));
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Remove a location
+        vm.prank(godmode);
+        TRSRY.removeAssetLocation(address(reserve), allocator_);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.locations.length, 0);
+    }
+
+    function testFuzz_removeAssetLocation_onePreviousLocation(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+        vm.assume(allocator_ != address(1));
+
+        address[] memory locations = new address[](1);
+        locations[0] = address(1);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Remove a location
+        vm.prank(godmode);
+        TRSRY.removeAssetLocation(address(reserve), allocator_);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.locations.length, 1);
+        assertEq(asset.locations[0], address(1));
+    }
+
+    function testFuzz_removeAssetLocation_manyPreviousLocations(address allocator_) public {
+        vm.assume(allocator_ != address(0));
+        vm.assume(allocator_ != address(1));
+        vm.assume(allocator_ != address(2));
+
+        address[] memory locations = new address[](2);
+        locations[0] = address(1);
+        locations[1] = address(2);
+        // Add an asset
+        vm.prank(godmode);
+        TRSRY.addAsset(address(reserve), locations);
+
+        // Add a location
+        vm.prank(godmode);
+        TRSRY.addAssetLocation(address(reserve), allocator_);
+
+        // Remove a location
+        vm.prank(godmode);
+        TRSRY.removeAssetLocation(address(reserve), allocator_);
+
+        // Verify asset data
+        TRSRYv1_1.Asset memory asset = TRSRY.getAssetData(address(reserve));
+        assertEq(asset.locations.length, 2);
+        assertEq(asset.locations[0], address(1));
+        assertEq(asset.locations[1], address(2));
+    }
+
+    function testRevert_removeAssetLocation_AssetNotApproved() public {
+        /// Try to remove the location of an asset which is not approved
+        bytes memory err = abi.encodeWithSignature("TRSRY_AssetNotApproved(address)", reserve);
+        vm.expectRevert(err);
+        vm.prank(godmode);
+        TRSRY.removeAssetLocation(address(reserve), address(1));
+    }
 }
