7a6b0da7e5861d4457dccfda585b635ffee58edd
Jem <0x0xjem@gmail.com>
2023-11-14 13:37:50 +0400

    chore: linting

--- src/modules/SPPLY/SPPLY.v1.sol
+++ src/modules/SPPLY/SPPLY.v1.sol
@@ -251,7 +251,9 @@ abstract contract SPPLYv1 is ModuleWithSubmodules {
     function storeCategorySupply(Category category_) external virtual;
 
     /// @notice             Returns the underlying reserves for a category
-    function getReservesByCategory(Category category_) external view virtual returns (Reserves[] memory);
+    function getReservesByCategory(
+        Category category_
+    ) external view virtual returns (Reserves[] memory);
 
     //============================================================================================//
     //                                       SUPPLY METRICS                                       //
