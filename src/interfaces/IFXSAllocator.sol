f2a33557b5acba12ff60477f11c0a0d7bf6a38ff
Jem <0x0xjem@gmail.com>
2023-12-06 12:37:17 +0400

    TRSRY: Disable migration of soon-to-be-liquidated token balances. Set debt for DAI and veFXS.

---
+++ src/interfaces/IFXSAllocator.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.15;
+
+/// @notice     Interface for the FXS Allocator
+/// @dev        This contains the bare minimum functionality required
+abstract contract IFXSAllocator {
+    uint256 public totalAmountDeployed;
+}
--- src/scripts/ops/batches/RBSv2Install_1.sol
+++ src/scripts/ops/batches/RBSv2Install_1.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.15;
 
 import {console2} from "forge-std/console2.sol";
+import {StdAssertions} from "forge-std/StdAssertions.sol";
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
@@ -11,11 +12,16 @@ import "src/Kernel.sol";
 import {OlympusTreasury} from "modules/TRSRY/OlympusTreasury.sol";
 import {TreasuryCustodian} from "policies/TreasuryCustodian.sol";
 
-contract RBSv2Install_1 is OlyBatch {
+import {IFXSAllocator} from "interfaces/IFXSAllocator.sol";
+
+contract RBSv2Install_1 is OlyBatch, StdAssertions {
     // Existing Olympus contracts
     address kernel;
     address trsry;
     address treasuryCustodian;
+    address clearinghouseV1;
+    address clearinghouseV1_1;
+    address vefxsallocator;
 
     // Tokens
     address dai;
@@ -24,6 +30,8 @@ contract RBSv2Install_1 is OlyBatch {
     address wsteth;
     address bal;
     address aura;
+    address fxs;
+    address vefxs;
 
     // New contracts
     address newTrsry;
@@ -32,6 +40,9 @@ contract RBSv2Install_1 is OlyBatch {
         kernel = envAddress("current", "olympus.Kernel");
         trsry = envAddress("last", "olympus.modules.OlympusTreasury");
         treasuryCustodian = envAddress("current", "olympus.policies.TreasuryCustodian");
+        clearinghouseV1 = envAddress("current", "olympus.policies.ClearinghouseV1");
+        clearinghouseV1_1 = envAddress("current", "olympus.policies.ClearinghouseV1_1");
+        vefxsallocator = envAddress("current", "olympus.legacy.veFXSAllocator");
 
         dai = envAddress("current", "external.tokens.DAI");
         sdai = envAddress("current", "external.tokens.sDAI");
@@ -39,6 +50,8 @@ contract RBSv2Install_1 is OlyBatch {
         wsteth = envAddress("current", "external.tokens.wstETH");
         bal = envAddress("current", "external.tokens.BAL");
         aura = envAddress("current", "external.tokens.AURA");
+        fxs = envAddress("current", "external.tokens.FXS");
+        vefxs = envAddress("current", "external.tokens.veFXS");
 
         newTrsry = envAddress("current", "olympus.modules.OlympusTreasury");
     }
@@ -46,43 +59,115 @@ contract RBSv2Install_1 is OlyBatch {
     function RBSv2Install_1_1(bool send_) external isDaoBatch(send_) {
         // This DAO MS batch:
         // 1. Transfers all tokens from the old treasury to the new treasury
-        // 2. Upgrades the OlympusTreasury contract to the new version
+        // 2. Records the current debt of the old treasury
+        // 3. Upgrades the OlympusTreasury contract to the new version
+        // 4. Sets debt on the new treasury contract
 
         // 1. Transfer all tokens from the old treasury to the new treasury
-        uint256 daiBalance = ERC20(dai).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, dai, daiBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, dai, daiBalance));
-        console2.log("Transfered DAI: %s", daiBalance);
+        // DAI
+        {
+            uint256 daiBalance = ERC20(dai).balanceOf(trsry);
+            addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, dai, daiBalance));
+            addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, dai, daiBalance));
+            console2.log("Transfered DAI: %s", daiBalance);
+        }
 
-        uint256 sdaiBalance = ERC20(sdai).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, sdai, sdaiBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, sdai, sdaiBalance));
-        console2.log("Transfered sDAI: %s", sdaiBalance);
+        // sDAI
+        {
+            uint256 sdaiBalance = ERC20(sdai).balanceOf(trsry);
+            addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, sdai, sdaiBalance));
+            addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, sdai, sdaiBalance));
+            console2.log("Transfered sDAI: %s", sdaiBalance);
+        }
 
-        uint256 lusdBalance = ERC20(lusd).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, lusd, lusdBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, lusd, lusdBalance));
-        console2.log("Transfered LUSD: %s", lusdBalance);
+        // The following assets exist in TRSRY, but will be swapped for DAI
 
-        uint256 wstethBalance = ERC20(wsteth).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, wsteth, wstethBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, wsteth, wstethBalance));
-        console2.log("Transfered wstETH: %s", wstethBalance);
+        // uint256 lusdBalance = ERC20(lusd).balanceOf(trsry);
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, lusd, lusdBalance));
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, lusd, lusdBalance));
+        // console2.log("Transfered LUSD: %s", lusdBalance);
 
-        uint256 balBalance = ERC20(bal).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, bal, balBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, bal, balBalance));
-        console2.log("Transfered BAL: %s", balBalance);
+        // uint256 wstethBalance = ERC20(wsteth).balanceOf(trsry);
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, wsteth, wstethBalance));
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, wsteth, wstethBalance));
+        // console2.log("Transfered wstETH: %s", wstethBalance);
 
-        uint256 auraBalance = ERC20(aura).balanceOf(trsry);
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, aura, auraBalance));
-        addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, aura, auraBalance));
-        console2.log("Transfered AURA: %s", auraBalance);
+        // uint256 balBalance = ERC20(bal).balanceOf(trsry);
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, bal, balBalance));
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, bal, balBalance));
+        // console2.log("Transfered BAL: %s", balBalance);
 
-        // 2. Upgrade the OlympusTreasury contract to the new version
-        addToBatch(kernel, abi.encodeWithSelector(Kernel.executeAction.selector, Actions.UpgradeModule, newTrsry));
-        console2.log("Upgraded OlympusTreasury to new version: %s", newTrsry);
+        // uint256 auraBalance = ERC20(aura).balanceOf(trsry);
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.grantWithdrawerApproval.selector, treasuryCustodian, aura, auraBalance));
+        // addToBatch(treasuryCustodian, abi.encodeWithSelector(TreasuryCustodian.withdrawReservesTo.selector, newTrsry, aura, auraBalance));
+        // console2.log("Transfered AURA: %s", auraBalance);
 
-        // TODO setDebt?
+        // 2. Record the current debt of the old treasury
+        OlympusTreasury trsryModule = OlympusTreasury(trsry);
+        
+        // DAI
+        // - Clearinghouse debt is denominated in DAI
+        uint256 daiClearinghouseV1Debt;
+        uint256 daiClearinghouseV1_1Debt;
+        {
+            ERC20 daiToken = ERC20(dai);
+            uint256 daiTotalDebt = trsryModule.totalDebt(daiToken);
+            daiClearinghouseV1Debt = trsryModule.reserveDebt(daiToken, clearinghouseV1);
+            daiClearinghouseV1_1Debt = trsryModule.reserveDebt(daiToken, clearinghouseV1_1);
+            assertEq(daiTotalDebt, daiClearinghouseV1Debt + daiClearinghouseV1_1Debt, "Clearinghouse debt should equal total debt");
+        }
+
+        // sDAI
+        {
+            ERC20 sdaiToken = ERC20(sdai);
+            uint256 sdaiTotalDebt = trsryModule.totalDebt(sdaiToken);
+            assertEq(sdaiTotalDebt, 0, "sDAI debt should be 0");
+        }
+
+        // FXS
+        {
+            ERC20 fxsToken = ERC20(fxs);
+            uint256 fxsTotalDebt = trsryModule.totalDebt(fxsToken);
+            assertEq(fxsTotalDebt, 0, "FXS debt should be 0");
+        }
+
+        // veFXS is in an allocator, have no balance in the current treasury, but also don't have any debt value set
+        uint256 vefxsBalance;
+        {
+            IFXSAllocator vefxsAllocatorContract = IFXSAllocator(vefxsallocator);
+            ERC20 vefxsToken = ERC20(vefxs);
+            vefxsBalance = vefxsAllocatorContract.totalAmountDeployed();
+
+            uint256 vefxsTotalDebt = trsryModule.totalDebt(vefxsToken);
+            assertEq(vefxsTotalDebt, 0, "FXS debt should be 0");
+        }
+
+        // 3. Upgrade the OlympusTreasury contract to the new version
+        {
+            addToBatch(kernel, abi.encodeWithSelector(Kernel.executeAction.selector, Actions.UpgradeModule, newTrsry));
+            console2.log("Upgraded OlympusTreasury to new version: %s", newTrsry);
+        }
+
+        // 4. Transfer debt over to the new treasury
+        OlympusTreasury newTrsryModule = OlympusTreasury(newTrsry);
+
+        // DAI
+        {
+            ERC20 daiToken = ERC20(dai);
+            addToBatch(newTrsry, abi.encodeWithSelector(OlympusTreasury.setDebt.selector, clearinghouseV1, daiToken, daiClearinghouseV1Debt));
+            addToBatch(newTrsry, abi.encodeWithSelector(OlympusTreasury.setDebt.selector, clearinghouseV1_1, daiToken, daiClearinghouseV1_1Debt));
+        }
+
+        // sDAI: no debt
+
+        // FXS: no debt
+
+        // veFXS
+        {
+            ERC20 vefxsToken = ERC20(vefxs);
+            addToBatch(newTrsry, abi.encodeWithSelector(OlympusTreasury.setDebt.selector, vefxsallocator, vefxsToken, vefxsBalance));
+        }
+
+        // TODO 5. Register TRSRY assets
     }
 }
