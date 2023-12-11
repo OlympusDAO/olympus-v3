f1b75ccef2307846a51e42ad5c110055c5975de4
Jem <0x0xjem@gmail.com>
2023-12-11 12:03:57 +0400

    Cross-port improvements to batch script

--- src/scripts/ops/batch.sh
+++ src/scripts/ops/batch.sh
@@ -5,8 +5,22 @@
 # 2. String: Batch Name
 # 3. Bool: Whether to send to STS or not (if false, just simulates the batch)
 
-# Load environment variables
+# Load environment variables, but respect overrides
+curenv=$(declare -p -x)
 source .env
+eval "$curenv"
+
+CONTRACT_NAME=$1
+BATCH_NAME=$2
+BROADCAST=$3
+
+echo "Contract name: $CONTRACT_NAME"
+
+echo "Batch name: $BATCH_NAME"
+
+echo "Broadcasting: $BROADCAST"
+
+echo "Using RPC at URL: $RPC_URL"
 
 # Execute the batch
-forge script ./src/scripts/ops/batches/$1.sol:$1 --sig "$2(bool)()" $3 --slow -vvv --sender $SIGNER_ADDRESS --rpc-url $RPC_URL
+forge script ./src/scripts/ops/batches/$CONTRACT_NAME.sol:$CONTRACT_NAME --sig "$BATCH_NAME(bool)()" $BROADCAST --slow -vvv --sender $SIGNER_ADDRESS --rpc-url $RPC_URL
