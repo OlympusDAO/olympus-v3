30d6360576fe11e99c10b73d9982afd4b8388665
Jem <0x0xjem@gmail.com>
2024-11-07 15:25:45 +0400

    Modify scripts to support MS batch execution on testnets

--- src/scripts/ops/OlyBatch.sol
+++ src/scripts/ops/OlyBatch.sol
@@ -3,9 +3,12 @@ pragma solidity 0.8.15;
 
 import {BatchScript} from "./lib/BatchScript.sol";
 import {stdJson} from "forge-std/StdJson.sol";
+import {console2} from "forge-std/console2.sol";
+import {Surl} from "surl-1.0.0/Surl.sol";
 
 abstract contract OlyBatch is BatchScript {
     using stdJson for string;
+    using Surl for *;
 
     string internal env;
     string internal chain;
@@ -81,4 +84,74 @@ abstract contract OlyBatch is BatchScript {
     }
 
     function loadEnv() internal virtual;
+
+    function executeBatch(bool send_) internal override {
+        bool isTestnet = vm.envOr("TESTNET", false);
+
+        if (isTestnet && send_) {
+            console2.log("Sending batch on testnet");
+            _sendTestnetBatch();
+            return;
+        }
+
+        super.executeBatch(send_);
+    }
+
+    function _sendTestnetBatch() private {
+        // Get the testnet RPC URL and access key
+        string memory TENDERLY_ACCOUNT_SLUG = vm.envString("TENDERLY_ACCOUNT_SLUG");
+        string memory TENDERLY_PROJECT_SLUG = vm.envString("TENDERLY_PROJECT_SLUG");
+        string memory TENDERLY_VNET_ID = vm.envString("TENDERLY_VNET_ID");
+        string memory TENDERLY_ACCESS_KEY = vm.envString("TENDERLY_ACCESS_KEY");
+
+        // Iterate over the proposal actions and execute them
+        for (uint256 i; i < actionsTo.length; i++) {
+            console2.log("Preparing proposal action ", i + 1);
+
+            // Construct the API call
+            string[] memory headers = new string[](3);
+            headers[0] = "Accept: application/json";
+            headers[1] = "Content-Type: application/json";
+            headers[2] = string.concat("X-Access-Key: ", TENDERLY_ACCESS_KEY);
+
+            string memory url = string.concat(
+                "https://api.tenderly.co/api/v1/account/",
+                TENDERLY_ACCOUNT_SLUG,
+                "/project/",
+                TENDERLY_PROJECT_SLUG,
+                "/vnets/",
+                TENDERLY_VNET_ID,
+                "/transactions"
+            );
+
+            // Execute the API call
+            // solhint-disable quotes
+            console2.log("Executing proposal action ", i + 1);
+            (uint256 status, bytes memory response) = url.post(
+                headers,
+                string.concat(
+                    "{",
+                    '"callArgs": {',
+                    '"from": "',
+                    vm.toString(daoMS),
+                    '", "to": "',
+                    vm.toString(actionsTo[i]),
+                    '", "gas": "0x7a1200", "gasPrice": "0x10", "value": "0x0", ',
+                    '"data": "',
+                    vm.toString(actionsData[i]),
+                    '"',
+                    "}}"
+                )
+            );
+            // solhint-enable quotes
+
+            string memory responseString = string(response);
+            console2.log("Response: ", responseString);
+
+            // If the response contains "error", exit
+            if (status >= 400 || vm.keyExists(responseString, ".error")) {
+                revert("Error executing proposal action");
+            }
+        }
+    }
 }
--- src/scripts/ops/README.md
+++ src/scripts/ops/README.md
@@ -4,6 +4,10 @@ This directory contains batch scripts for the Olympus DAO multisig.
 
 ## Fork Testing
 
-There is no way to directly run the scripts (in their current state) on a forked chain.
+To run the scripts on a testnet/forked chain, provide the `--testnet` flag to the `batch.sh` script. This requires certain environment variables to be set, which are documented in the `batch.sh` file.
 
-If you are using Tenderly, you can run each step of the script manually through the "Send Transaction" feature on the dashboard.
+For example:
+
+```bash
+./batch.sh --contract ContractRegistryInstall --batch script1_install --broadcast true --testnet true --env .env.testnet
+```
