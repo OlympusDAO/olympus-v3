748fbda6ef3125aac833185d9fedc030513be52d
Jem <0x0xjem@gmail.com>
2024-11-13 14:39:05 +0400

    chore: linting

--- shell/change_price.sh
+++ shell/change_price.sh
@@ -3,11 +3,11 @@
 source .env
 
 # User-provided input ($1) is the dollar price of OHM
-echo "Setting OHM price to \$$1 on Testnet.";
-echo "";
+echo "Setting OHM price to \$$1 on Testnet."
+echo ""
 
 # Convert dollar price of OHM into price feed value (using ETH = $1500)
-OHM_ETH_PRICE=$(echo "$1*1000000000000000000/1500" | bc);
+OHM_ETH_PRICE=$(echo "$1*1000000000000000000/1500" | bc)
 
 # DAI price feed value is fixed using ETH = $1500
 DAI_ETH_PRICE=666666666666667
@@ -16,27 +16,27 @@ DAI_ETH_PRICE=666666666666667
 TIMESTAMP=$(date +%s)
 
 # Set price feed values
-echo "Updating the OHM price feed...";
-cast send --private-key=$POLICY_PRIVATE_KEY --rpc-url=$RPC_URL --from=$POLICY_ADDRESS --chain=$CHAIN $OHM_ETH_FEED "setLatestAnswer(int256)()" $OHM_ETH_PRICE > /dev/null;
-cast send --private-key=$POLICY_PRIVATE_KEY --rpc-url=$RPC_URL --from=$POLICY_ADDRESS --chain=$CHAIN $OHM_ETH_FEED "setTimestamp(uint256)()" $TIMESTAMP > /dev/null;
-echo "Complete.";
-echo "";
+echo "Updating the OHM price feed..."
+cast send --private-key=$POLICY_PRIVATE_KEY --rpc-url=$RPC_URL --from=$POLICY_ADDRESS --chain=$CHAIN $OHM_ETH_FEED "setLatestAnswer(int256)()" $OHM_ETH_PRICE > /dev/null
+cast send --private-key=$POLICY_PRIVATE_KEY --rpc-url=$RPC_URL --from=$POLICY_ADDRESS --chain=$CHAIN $OHM_ETH_FEED "setTimestamp(uint256)()" $TIMESTAMP > /dev/null
+echo "Complete."
+echo ""
 
-echo "Updating the DAI price feed...";
-cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $DAI_ETH_FEED "setLatestAnswer(int256)" $DAI_ETH_PRICE > /dev/null;
-cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $DAI_ETH_FEED "setTimestamp(uint256)" $TIMESTAMP > /dev/null;
-echo "Complete.";
-echo "";
+echo "Updating the DAI price feed..."
+cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $DAI_ETH_FEED "setLatestAnswer(int256)" $DAI_ETH_PRICE > /dev/null
+cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $DAI_ETH_FEED "setTimestamp(uint256)" $TIMESTAMP > /dev/null
+echo "Complete."
+echo ""
 
 # Reset heart and call heart.beat()
-echo "Resetting the heart...";
-cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $HEART "resetBeat()" > /dev/null;
-echo "Complete.";
-echo "";
+echo "Resetting the heart..."
+cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $HEART "resetBeat()" > /dev/null
+echo "Complete."
+echo ""
 
-echo "Calling heart.beat()...";
-cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $HEART "beat()" > /dev/null;
-echo "Complete.";
-echo "";
+echo "Calling heart.beat()..."
+cast send --private-key=$POLICY_PRIVATE_KEY --from=$POLICY_ADDRESS --rpc-url=$RPC_URL --chain=$CHAIN $HEART "beat()" > /dev/null
+echo "Complete."
+echo ""
 
-echo "Price update complete and Operator state updated.";
+echo "Price update complete and Operator state updated."
--- src/scripts/ops/batch.sh
+++ src/scripts/ops/batch.sh
@@ -16,12 +16,12 @@ set -e
 # Iterate through named arguments
 # Source: https://unix.stackexchange.com/a/388038
 while [ $# -gt 0 ]; do
-  if [[ $1 == *"--"* ]]; then
-    v="${1/--/}"
-    declare $v="$2"
-  fi
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
 
-  shift
+    shift
 done
 
 # Get the name of the .env file or use the default
@@ -29,9 +29,9 @@ ENV_FILE=${env:-".env"}
 echo "Sourcing environment variables from $ENV_FILE"
 
 # Load environment file
-set -a  # Automatically export all variables
+set -a # Automatically export all variables
 source $ENV_FILE
-set +a  # Disable automatic export
+set +a # Disable automatic export
 
 # Set sane defaults
 BROADCAST=${broadcast:-false}
@@ -39,26 +39,26 @@ TESTNET=${testnet:-false}
 
 # Check if contract is set
 if [ -z "$contract" ]; then
-  echo "No contract name provided. Provide the contract name after the --contract flag."
-  exit 1
+    echo "No contract name provided. Provide the contract name after the --contract flag."
+    exit 1
 fi
 
 # Check if batch is set
 if [ -z "$batch" ]; then
-  echo "No batch name provided. Provide the batch name after the --batch flag."
-  exit 1
+    echo "No batch name provided. Provide the batch name after the --batch flag."
+    exit 1
 fi
 
 # Check if RPC_URL is set
 if [ -z "$RPC_URL" ]; then
-  echo "No RPC URL provided. Specify the RPC_URL in the $ENV_FILE file."
-  exit 1
+    echo "No RPC URL provided. Specify the RPC_URL in the $ENV_FILE file."
+    exit 1
 fi
 
 # Check if SIGNER_ADDRESS is set
 if [ -z "$SIGNER_ADDRESS" ]; then
-  echo "No signer address provided. Specify the SIGNER_ADDRESS in the $ENV_FILE file."
-  exit 1
+    echo "No signer address provided. Specify the SIGNER_ADDRESS in the $ENV_FILE file."
+    exit 1
 fi
 
 echo "Contract name: $contract"
