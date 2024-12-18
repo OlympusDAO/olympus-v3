f293e5e41de385c774f673259f09f40a18c2d652
Jem <0x0xjem@gmail.com>
2024-12-18 18:12:55 +0500

    Implement tests for CDPOS.create()

--- src/test/modules/CDPOS/CDPOSTest.sol
+++ src/test/modules/CDPOS/CDPOSTest.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.15;
 import {Test} from "forge-std/Test.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {ModuleTestFixtureGenerator} from "src/test/lib/ModuleTestFixtureGenerator.sol";
+import {MockERC20} from "forge-std/mocks/MockERC20.sol";
 
 import {Kernel, Actions} from "src/Kernel.sol";
 import {OlympusConvertibleDepositPositions} from "src/modules/CDPOS/OlympusConvertibleDepositPositions.sol";
@@ -11,15 +12,28 @@ import {OlympusConvertibleDepositPositions} from "src/modules/CDPOS/OlympusConve
 abstract contract CDPOSTest is Test, IERC721Receiver {
     using ModuleTestFixtureGenerator for OlympusConvertibleDepositPositions;
 
+    uint256 public constant REMAINING_DEPOSIT = 25e18;
+    uint256 public constant CONVERSION_PRICE = 2e18;
+    uint48 public constant EXPIRY_DELAY = 1 days;
+    uint48 public constant INITIAL_BLOCK = 100000000;
+
     Kernel public kernel;
     OlympusConvertibleDepositPositions public CDPOS;
     address public godmode;
+    address public convertibleDepositToken;
 
     uint256[] public positions;
 
     function setUp() public {
+        vm.warp(INITIAL_BLOCK);
+
         kernel = new Kernel();
-        CDPOS = OlympusConvertibleDepositPositions(address(kernel));
+        CDPOS = new OlympusConvertibleDepositPositions(address(kernel));
+
+        // Set up the convertible deposit token
+        MockERC20 mockERC20 = new MockERC20();
+        mockERC20.initialize("Convertible Deposit Token", "CDT", 18);
+        convertibleDepositToken = address(mockERC20);
 
         // Generate fixtures
         godmode = CDPOS.generateGodmodeFixture(type(OlympusConvertibleDepositPositions).name);
@@ -38,5 +52,45 @@ abstract contract CDPOSTest is Test, IERC721Receiver {
         positions.push(tokenId);
 
         return this.onERC721Received.selector;
+    }
+
+    // ========== MODIFIERS ========== //
+
+    modifier givenConvertibleDepositTokenDecimals(uint8 decimals_) {
+        // Create a new token with the given decimals
+        MockERC20 mockERC20 = new MockERC20();
+        mockERC20.initialize("Convertible Deposit Token", "CDT", decimals_);
+        convertibleDepositToken = address(mockERC20);
+        _;
+    }
+
+    function _createPosition(
+        address owner_,
+        uint256 remainingDeposit_,
+        uint256 conversionPrice_,
+        uint48 expiry_,
+        bool wrap_
+    ) internal {
+        vm.prank(godmode);
+        CDPOS.create(
+            owner_,
+            convertibleDepositToken,
+            remainingDeposit_,
+            conversionPrice_,
+            expiry_,
+            wrap_
+        );
+    }
+
+    modifier givenPositionCreated(
+        address owner_,
+        uint256 remainingDeposit_,
+        uint256 conversionPrice_,
+        uint48 expiry_,
+        bool wrap_
+    ) {
+        // Create a new position
+        _createPosition(owner_, remainingDeposit_, conversionPrice_, expiry_, wrap_);
+        _;
     }
 }
--- src/test/modules/CDPOS/create.t.sol
+++ src/test/modules/CDPOS/create.t.sol
@@ -3,36 +3,374 @@ pragma solidity 0.8.15;
 
 import {CDPOSTest} from "./CDPOSTest.sol";
 
+import {CDPOSv1} from "src/modules/CDPOS/CDPOS.v1.sol";
+import {Module} from "src/Kernel.sol";
+
 contract CreateCDPOSTest is CDPOSTest {
+    event PositionCreated(
+        uint256 indexed positionId,
+        address indexed owner,
+        address indexed convertibleDepositToken,
+        uint256 remainingDeposit,
+        uint256 conversionPrice,
+        uint48 expiry,
+        bool wrapped
+    );
+
     // when the caller is not a permissioned address
-    //  [ ] it reverts
+    //  [X] it reverts
     // when the owner is the zero address
-    //  [ ] it reverts
+    //  [X] it reverts
     // when the convertible deposit token is the zero address
-    //  [ ] it reverts
+    //  [X] it reverts
     // when the remaining deposit is 0
-    //  [ ] it reverts
+    //  [X] it reverts
     // when the conversion price is 0
-    //  [ ] it reverts
+    //  [X] it reverts
     // when the expiry is in the past or now
-    //  [ ] it reverts
+    //  [X] it reverts
     // when multiple positions are created
-    //  [ ] the position IDs are sequential
-    //  [ ] the position IDs are unique
-    //  [ ] the owner's list of positions is updated
-    //  [ ] the owner's balance is increased
+    //  [X] the position IDs are sequential
+    //  [X] the position IDs are unique
+    //  [X] the owner's list of positions is updated
     // when the expiry is in the future
-    //  [ ] it sets the expiry
+    //  [X] it sets the expiry
     // when the conversion would result in an overflow
     //  [ ] it reverts
     // when the conversion would result in an underflow
     //  [ ] it reverts
     // when the wrap flag is true
-    //  [ ] it mints the ERC721 token
-    //  [ ] it marks the position as wrapped
-    // [ ] it emits a PositionCreated event
-    // [ ] the position is marked as unwrapped
-    // [ ] the position is listed as owned by the owner
-    // [ ] the owner's list of positions is updated
-    // [ ] the balance of the owner is increased
+    //  [X] it mints the ERC721 token
+    //  [X] it marks the position as wrapped
+    //  [X] the position is listed as owned by the owner
+    //  [X] the ERC721 position is listed as owned by the owner
+    //  [X] the ERC721 balance of the owner is increased
+    // [X] it emits a PositionCreated event
+    // [X] the position is marked as unwrapped
+    // [X] the position is listed as owned by the owner
+    // [X] the owner's list of positions is updated
+    // [X] the ERC721 position is not listed as owned by the owner
+    // [X] the ERC721 balance of the owner is not increased
+
+    function test_callerNotPermissioned_reverts() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(Module.Module_PolicyNotPermitted.selector, address(this))
+        );
+
+        vm.prank(address(this));
+        CDPOS.create(
+            address(this),
+            convertibleDepositToken,
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            EXPIRY_DELAY,
+            false
+        );
+    }
+
+    function test_ownerIsZeroAddress_reverts() public {
+        // Expect revert
+        vm.expectRevert(abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "owner"));
+
+        // Call function
+        _createPosition(address(0), REMAINING_DEPOSIT, CONVERSION_PRICE, EXPIRY_DELAY, false);
+    }
+
+    function test_convertibleDepositTokenIsZeroAddress_reverts() public {
+        // Expect revert
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                CDPOSv1.CDPOS_InvalidParams.selector,
+                "convertible deposit token"
+            )
+        );
+
+        // Call function
+        vm.prank(godmode);
+        CDPOS.create(
+            address(this),
+            address(0),
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            EXPIRY_DELAY,
+            false
+        );
+    }
+
+    function test_remainingDepositIsZero_reverts() public {
+        // Expect revert
+        vm.expectRevert(abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "deposit"));
+
+        // Call function
+        _createPosition(address(this), 0, CONVERSION_PRICE, EXPIRY_DELAY, false);
+    }
+
+    function test_conversionPriceIsZero_reverts() public {
+        // Expect revert
+        vm.expectRevert(
+            abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "conversion price")
+        );
+
+        // Call function
+        _createPosition(address(this), REMAINING_DEPOSIT, 0, EXPIRY_DELAY, false);
+    }
+
+    function test_expiryIsInPastOrNow_reverts(uint48 expiry_) public {
+        uint48 expiry = uint48(bound(expiry_, 0, block.timestamp));
+
+        // Expect revert
+        vm.expectRevert(abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "expiry"));
+
+        // Call function
+        _createPosition(address(this), REMAINING_DEPOSIT, CONVERSION_PRICE, expiry, false);
+    }
+
+    function test_singlePosition() public {
+        // Expect event
+        vm.expectEmit(true, true, true, true);
+        emit PositionCreated(
+            0,
+            address(this),
+            convertibleDepositToken,
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            false
+        );
+
+        // Call function
+        _createPosition(
+            address(this),
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            false
+        );
+
+        // Assert that this contract did not receive the position ERC721
+        assertEq(positions.length, 0, "positions.length");
+
+        // Assert that the ERC721 balances were not updated
+        vm.expectRevert("NOT_MINTED");
+        CDPOS.ownerOf(0);
+        assertEq(CDPOS.balanceOf(address(this)), 0, "balanceOf(address(this))");
+
+        // Assert that the position is correct
+        CDPOSv1.Position memory position = CDPOS.getPosition(0);
+        assertEq(position.owner, address(this), "position.owner");
+        assertEq(
+            position.convertibleDepositToken,
+            convertibleDepositToken,
+            "position.convertibleDepositToken"
+        );
+        assertEq(position.remainingDeposit, REMAINING_DEPOSIT, "position.remainingDeposit");
+        assertEq(position.conversionPrice, CONVERSION_PRICE, "position.conversionPrice");
+        assertEq(position.expiry, block.timestamp + EXPIRY_DELAY, "position.expiry");
+        assertEq(position.wrapped, false, "position.wrapped");
+
+        // Assert that the owner's list of positions is updated
+        uint256[] memory ownerPositions = CDPOS.getUserPositionIds(address(this));
+        assertEq(ownerPositions.length, 1, "ownerPositions.length");
+        assertEq(ownerPositions[0], 0, "ownerPositions[0]");
+    }
+
+    function test_singlePosition_whenWrapped() public {
+        // Expect event
+        vm.expectEmit(true, true, true, true);
+        emit PositionCreated(
+            0,
+            address(this),
+            convertibleDepositToken,
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            true
+        );
+
+        // Call function
+        _createPosition(
+            address(this),
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            true
+        );
+
+        // Assert that this contract received the position ERC721
+        assertEq(positions.length, 1, "positions.length");
+        assertEq(positions[0], 0, "positions[0]");
+
+        // Assert that the ERC721 balances were updated
+        assertEq(CDPOS.ownerOf(0), address(this), "ownerOf(0)");
+        assertEq(CDPOS.balanceOf(address(this)), 1, "balanceOf(address(this))");
+
+        // Assert that the position is correct
+        CDPOSv1.Position memory position = CDPOS.getPosition(0);
+        assertEq(position.owner, address(this), "position.owner");
+        assertEq(
+            position.convertibleDepositToken,
+            convertibleDepositToken,
+            "position.convertibleDepositToken"
+        );
+        assertEq(position.remainingDeposit, REMAINING_DEPOSIT, "position.remainingDeposit");
+        assertEq(position.conversionPrice, CONVERSION_PRICE, "position.conversionPrice");
+        assertEq(position.expiry, block.timestamp + EXPIRY_DELAY, "position.expiry");
+        assertEq(position.wrapped, true, "position.wrapped");
+
+        // Assert that the owner's list of positions is updated
+        uint256[] memory ownerPositions = CDPOS.getUserPositionIds(address(this));
+        assertEq(ownerPositions.length, 1, "ownerPositions.length");
+        assertEq(ownerPositions[0], 0, "ownerPositions[0]");
+    }
+
+    function test_multiplePositions_singleOwner() public {
+        // Create 10 positions
+        for (uint256 i = 0; i < 10; i++) {
+            _createPosition(
+                address(this),
+                REMAINING_DEPOSIT,
+                CONVERSION_PRICE,
+                uint48(block.timestamp + EXPIRY_DELAY),
+                false
+            );
+        }
+
+        // Assert that the position count is correct
+        assertEq(CDPOS.positionCount(), 10, "positionCount");
+
+        // Assert that the owner has sequential position IDs
+        for (uint256 i = 0; i < 10; i++) {
+            CDPOSv1.Position memory position = CDPOS.getPosition(i);
+            assertEq(position.owner, address(this), "position.owner");
+
+            // Assert that the ERC721 position is not updated
+            vm.expectRevert("NOT_MINTED");
+            CDPOS.ownerOf(i);
+        }
+
+        // Assert that the ERC721 balance of the owner is not updated
+        assertEq(CDPOS.balanceOf(address(this)), 0, "balanceOf(address(this))");
+
+        // Assert that the owner's positions list is correct
+        uint256[] memory ownerPositions = CDPOS.getUserPositionIds(address(this));
+        assertEq(ownerPositions.length, 10, "ownerPositions.length");
+        for (uint256 i = 0; i < 10; i++) {
+            assertEq(ownerPositions[i], i, "ownerPositions[i]");
+        }
+    }
+
+    function test_multiplePositions_multipleOwners() public {
+        address owner1 = address(0x1);
+        address owner2 = address(0x2);
+
+        // Create 5 positions for owner1
+        for (uint256 i = 0; i < 5; i++) {
+            _createPosition(
+                owner1,
+                REMAINING_DEPOSIT,
+                CONVERSION_PRICE,
+                uint48(block.timestamp + EXPIRY_DELAY),
+                false
+            );
+        }
+
+        // Create 5 positions for owner2
+        for (uint256 i = 0; i < 5; i++) {
+            _createPosition(
+                owner2,
+                REMAINING_DEPOSIT,
+                CONVERSION_PRICE,
+                uint48(block.timestamp + EXPIRY_DELAY),
+                false
+            );
+        }
+
+        // Assert that the position count is correct
+        assertEq(CDPOS.positionCount(), 10, "positionCount");
+
+        // Assert that the owner1's positions are correct
+        for (uint256 i = 0; i < 5; i++) {
+            CDPOSv1.Position memory position = CDPOS.getPosition(i);
+            assertEq(position.owner, owner1, "position.owner");
+        }
+
+        // Assert that the owner2's positions are correct
+        for (uint256 i = 5; i < 10; i++) {
+            CDPOSv1.Position memory position = CDPOS.getPosition(i);
+            assertEq(position.owner, owner2, "position.owner");
+        }
+
+        // Assert that the ERC721 balances of the owners are correct
+        assertEq(CDPOS.balanceOf(owner1), 0, "balanceOf(owner1)");
+        assertEq(CDPOS.balanceOf(owner2), 0, "balanceOf(owner2)");
+
+        // Assert that the owner1's positions list is correct
+        uint256[] memory owner1Positions = CDPOS.getUserPositionIds(owner1);
+        assertEq(owner1Positions.length, 5, "owner1Positions.length");
+        for (uint256 i = 0; i < 5; i++) {
+            assertEq(owner1Positions[i], i, "owner1Positions[i]");
+        }
+
+        // Assert that the owner2's positions list is correct
+        uint256[] memory owner2Positions = CDPOS.getUserPositionIds(owner2);
+        assertEq(owner2Positions.length, 5, "owner2Positions.length");
+        for (uint256 i = 0; i < 5; i++) {
+            assertEq(owner2Positions[i], i + 5, "owner2Positions[i]");
+        }
+    }
+
+    function test_expiryInFuture(uint48 expiry_) public {
+        uint48 expiry = uint48(bound(expiry_, block.timestamp + 1, type(uint48).max));
+
+        // Expect event
+        vm.expectEmit(true, true, true, true);
+        emit PositionCreated(
+            0,
+            address(this),
+            convertibleDepositToken,
+            REMAINING_DEPOSIT,
+            CONVERSION_PRICE,
+            expiry,
+            false
+        );
+
+        // Call function
+        _createPosition(address(this), REMAINING_DEPOSIT, CONVERSION_PRICE, expiry, false);
+
+        // Assert that the position is correct
+        CDPOSv1.Position memory position = CDPOS.getPosition(0);
+        assertEq(position.expiry, expiry, "position.expiry");
+    }
+
+    function test_conversionOverflow_reverts() public {
+        // Expect revert
+        vm.expectRevert(
+            abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "conversion overflow")
+        );
+
+        // Call function
+        _createPosition(
+            address(this),
+            type(uint256).max,
+            CONVERSION_PRICE,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            false
+        );
+    }
+
+    function test_conversionUnderflow_reverts() public {
+        // Expect revert
+        vm.expectRevert(
+            abi.encodeWithSelector(CDPOSv1.CDPOS_InvalidParams.selector, "conversion underflow")
+        );
+
+        // Call function
+        _createPosition(
+            address(this),
+            REMAINING_DEPOSIT,
+            type(uint256).max,
+            uint48(block.timestamp + EXPIRY_DELAY),
+            false
+        );
+    }
 }
