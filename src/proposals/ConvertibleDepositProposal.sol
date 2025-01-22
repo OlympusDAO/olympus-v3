074334c7b0e4641a927e606e46f62d882b795a4c
Jem <0x0xjem@gmail.com>
2025-01-22 13:14:57 +0400

    OCG: Validation of end state

--- src/proposals/ConvertibleDepositProposal.sol
+++ src/proposals/ConvertibleDepositProposal.sol
@@ -10,6 +10,7 @@ import {ProposalScript} from "src/proposals/ProposalScript.sol";
 
 // Contracts
 import {Kernel} from "src/Kernel.sol";
+import {ROLESv1} from "src/modules/roles/ROLESv1.sol";
 
 /// @notice Activates the Convertible Deposit contracts
 contract ConvertibleDepositProposal is GovernorBravoProposal {
@@ -75,8 +76,13 @@ contract ConvertibleDepositProposal is GovernorBravoProposal {
 
         // Activate the Heart policy
 
+        // Activate the ConvertibleDepositFacility contract functionality
+
+        // TODO can we initialize the CDAuctioneer and EmissionManager policies here?
+
         // Next steps:
         // - DAO MS needs to initialize the new EmissionManager policy
+        // - DAO MS needs to initialize the new CDAuctioneer policy
     }
 
     // Executes the proposal actions.
@@ -92,17 +98,116 @@ contract ConvertibleDepositProposal is GovernorBravoProposal {
 
     // Validates the post-execution state.
     function _validate(Addresses addresses, address) internal view override {
-        // Validate that the Timelock has the "emissions_admin" role
-        // Validate that the DAO MS has the "emissions_admin" role
-        // Validate that the Timelock has the "cd_admin" role
-        // Validate that the Heart has the "heart" role
-        // Validate that the EmissionManager has the "cd_emissionmanager" role
-        // Validate that the CDAuctioneer has the "cd_auctioneer" role
+        // Load the contract addresses
+        ROLESv1 roles = ROLESv1(addresses.getAddress("olympus-module-roles"));
+        address timelock = addresses.getAddress("olympus-timelock");
+        address daoMS = addresses.getAddress("olympus-multisig-dao");
+
+        address heartOld = addresses.getAddress("olympus-policy-heart-1_6");
+        address heart = addresses.getAddress("olympus-policy-heart-1_7");
+        address emissionManagerOld = addresses.getAddress("olympus-policy-emissionmanager");
+        address emissionManager = addresses.getAddress("olympus-policy-emissionmanager-1_2");
+        address cdAuctioneer = addresses.getAddress(
+            "olympus-policy-convertible-deposit-auctioneer"
+        );
+        address cdFacility = addresses.getAddress("olympus-policy-convertible-deposit-facility");
+
+        // solhint-disable custom-errors
+
+        // Validate cleanup
         // Validate that the old Heart policy is disabled
+        require(
+            heartOld.isActive() == false,
+            "Old Heart policy is still active"
+        );
+
         // Validate that the old EmissionManager policy is disabled
+        require(
+            emissionManagerOld.isActive() == false,
+            "Old EmissionManager policy is still active"
+        );
+
+        // Validate that the "heart" role is revoked from the old Heart policy
+        require(
+            roles.hasRole(heartOld, bytes32("heart")) == false,
+            "Old Heart policy still has the heart role"
+        );
+
+        // Validate that the Timelock has the "emissions_admin" role
+        require(
+            roles.hasRole(timelock, bytes32("emissions_admin")) == true,
+            "Timelock does not have the emissions_admin role"
+        );
+
+        // Validate that the DAO MS has the "emissions_admin" role
+        require(
+            roles.hasRole(daoMS, bytes32("emissions_admin")) == true,
+            "DAO MS does not have the emissions_admin role"
+        );
+
+        // Validate that the Timelock has the "cd_admin" role
+        require(
+            roles.hasRole(timelock, bytes32("cd_admin")),
+            "Timelock does not have the cd_admin role"
+        );
+
+        // Validate that the DAO MS has the "cd_admin" role
+        require(
+            roles.hasRole(daoMS, bytes32("cd_admin")),
+            "DAO MS does not have the cd_admin role"
+        );
+
+        // Validate that the new Heart has the "heart" role
+        require(
+            roles.hasRole(heart, bytes32("heart")) == true,
+            "Heart policy does not have the heart role"
+        );
+
+        // Validate that the EmissionManager has the "cd_emissionmanager" role
+        require(
+            roles.hasRole(emissionManager, bytes32("cd_emissionmanager")) == true,
+            "EmissionManager policy does not have the cd_emissionmanager role"
+        );
+
+        // Validate that the CDAuctioneer has the "cd_auctioneer" role
+        require(
+            roles.hasRole(cdAuctioneer, bytes32("cd_auctioneer")) == true,
+            "CDAuctioneer policy does not have the cd_auctioneer role"
+        );
+
         // Validate that the new Heart policy is active
+        require(
+            heart.isActive() == true,
+            "Heart policy is not active"
+        );
+
         // Validate that the new EmissionManager policy is active
+        require(
+            emissionManager.isActive() == true,
+            "EmissionManager policy is not active"
+        );
+
         // Validate that the new ConvertibleDepositAuctioneer policy is active
+        require(
+            cdAuctioneer.isActive() == true,
+            "CDAuctioneer policy is not active"
+        );
+
         // Validate that the new ConvertibleDepositFacility policy is active
+        require(
+            cdFacility.isActive() == true,
+            "CDFacility policy is not active"
+        );
+
+        // Validate that the new ConvertibleDepositFacility policy is locally active
+        require(
+            cdFacility.locallyActive() == true,
+            "CDFacility policy is not locally active"
+        );
     }
+}
+
+// solhint-disable-next-line contract-name-camelcase
+contract ConvertibleDepositProposalScript is ProposalScript {
+    constructor() ProposalScript(new ConvertibleDepositProposal()) {}
 }
