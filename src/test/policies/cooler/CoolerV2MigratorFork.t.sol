91e112fef8609d402d7426876122c3336e82419a
Jem <0x0xjem@gmail.com>
2025-02-20 15:31:51 +0400

    Move away from fork test

--- src/test/policies/cooler/CoolerV2MigratorFork.t.sol
+++ src/test/policies/cooler/CoolerV2MigratorFork.t.sol
@@ -4,37 +4,126 @@ pragma solidity ^0.8.15;
 import {MonoCoolerBaseTest} from "./MonoCoolerBase.t.sol";
 
 import {Actions, Kernel} from "src/Kernel.sol";
+import {PolicyEnabler} from "src/policies/utils/PolicyEnabler.sol";
 import {CoolerV2Migrator} from "src/policies/cooler/CoolerV2Migrator.sol";
 import {Clearinghouse} from "src/policies/Clearinghouse.sol";
 import {Cooler} from "src/external/cooler/Cooler.sol";
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {MockERC4626} from "solmate/test/utils/mocks/MockERC4626.sol";
+import {MockDaiUsds} from "src/test/mocks/MockDaiUsds.sol";
+import {MockFlashloanLender} from "src/test/mocks/MockFlashloanLender.sol";
+import {OlympusClearinghouseRegistry} from "src/modules/CHREG/OlympusClearinghouseRegistry.sol";
+import {OlympusContractRegistry} from "src/modules/RGSTY/OlympusContractRegistry.sol";
+import {ContractRegistryAdmin} from "src/policies/ContractRegistryAdmin.sol";
+import {CoolerFactory} from "src/external/cooler/CoolerFactory.sol";
 
-contract CoolerV2MigratorForkTest is MonoCoolerBaseTest {
+contract CoolerV2MigratorTest is MonoCoolerBaseTest {
     CoolerV2Migrator internal migrator;
+    CoolerFactory internal coolerFactory;
     Clearinghouse internal clearinghouseUsds;
     Clearinghouse internal clearinghouseDai;
-    string internal RPC_URL = vm.envString("FORK_TEST_RPC_URL");
 
-    ERC20 internal dai;
+    MockERC20 internal dai;
+    MockERC4626 internal sDai;
+    MockDaiUsds internal daiMigrator;
+    MockFlashloanLender internal flashLender;
+
+    OlympusClearinghouseRegistry internal clearinghouseRegistry;
+    OlympusContractRegistry internal contractRegistry;
+    ContractRegistryAdmin internal contractRegistryAdmin;
+
+    address internal USER = makeAddr("user");
+
+    mapping(address => address) internal clearinghouseToCooler;
 
     function setUp() public virtual override {
-        // Fork
-        // After activation of the USDS Clearinghouse
-        // https://etherscan.io/tx/0x0afa12347eb2bf33b502f12db2db3b2ec261a673b57e51d4b85fd10169629458
-        vm.createSelectFork(RPC_URL, 21302624);
-
         // MonoCooler setup
         super.setUp();
 
-        // Clearinghouse setup
-        clearinghouseDai = Clearinghouse(0xE6343ad0675C9b8D3f32679ae6aDbA0766A2ab4c);
-        clearinghouseUsds = Clearinghouse(0x1e094fE00E13Fd06D64EeA4FB3cD912893606fE0);
-        dai = ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        // Tokens
+        dai = new MockERC20("DAI", "DAI", 18);
+        sDai = new MockERC4626(dai, "sDAI", "sDAI");
+
+        // Set up a mock DAI-USDS migrator
+        daiMigrator = new MockDaiUsds(dai, usds);
+
+        // Set up a mock flash loan lender
+        flashLender = new MockFlashloanLender(0, address(dai));
+
+        // Grant roles
+        rolesAdmin.grantRole("cooler_overseer", OVERSEER);
+        rolesAdmin.grantRole("emergency_shutdown", OVERSEER);
+        rolesAdmin.grantRole("contract_registry_admin", OVERSEER);
+
+        // Install additional modules
+        clearinghouseRegistry = new OlympusClearinghouseRegistry(
+            kernel,
+            address(0),
+            new address[](0)
+        );
+        contractRegistry = new OlympusContractRegistry(address(kernel));
+
+        vm.startPrank(EXECUTOR);
+        kernel.executeAction(Actions.InstallModule, address(clearinghouseRegistry));
+        kernel.executeAction(Actions.InstallModule, address(contractRegistry));
+        vm.stopPrank();
+
+        // Install ContractRegistryAdmin
+        contractRegistryAdmin = new ContractRegistryAdmin(address(kernel));
+
+        vm.startPrank(EXECUTOR);
+        kernel.executeAction(Actions.ActivatePolicy, address(contractRegistryAdmin));
+        vm.stopPrank();
+
+        // Install Clearinghouses
+        coolerFactory = new CoolerFactory();
+        clearinghouseDai = new Clearinghouse(
+            address(ohm),
+            address(gohm),
+            address(staking),
+            address(sDai),
+            address(coolerFactory),
+            address(kernel)
+        );
+        clearinghouseUsds = new Clearinghouse(
+            address(ohm),
+            address(gohm),
+            address(staking),
+            address(susds),
+            address(coolerFactory),
+            address(kernel)
+        );
+
+        vm.startPrank(EXECUTOR);
+        kernel.executeAction(Actions.ActivatePolicy, address(clearinghouseDai));
+        kernel.executeAction(Actions.ActivatePolicy, address(clearinghouseUsds));
+        vm.stopPrank();
+
+        // Activate & deactivate DAI clearinghouse
+        vm.startPrank(OVERSEER);
+        clearinghouseDai.activate();
+        clearinghouseDai.emergencyShutdown();
+        vm.stopPrank();
+
+        // Activate USDS clearinghouse
+        vm.startPrank(OVERSEER);
+        clearinghouseUsds.activate();
+        vm.stopPrank();
+
+        // Register contracts
+        vm.startPrank(OVERSEER);
+        contractRegistryAdmin.registerImmutableContract("dai", address(dai));
+        contractRegistryAdmin.registerImmutableContract("usds", address(usds));
+        contractRegistryAdmin.registerImmutableContract("gohm", address(gohm));
+        contractRegistryAdmin.registerContract("flash", address(flashLender));
+        contractRegistryAdmin.registerContract("dmgtr", address(daiMigrator));
+        vm.stopPrank();
 
         // CoolerV2Migrator setup
         migrator = new CoolerV2Migrator(address(kernel), address(cooler));
 
         // Install the policy
+        vm.prank(EXECUTOR);
         kernel.executeAction(Actions.ActivatePolicy, address(migrator));
 
         // Enable the policy
@@ -80,15 +169,29 @@ contract CoolerV2MigratorForkTest is MonoCoolerBaseTest {
         vm.prank(wallet_);
         address cooler = clearinghouse.factory().generateCooler(gohm, isUsds_ ? usds : dai);
 
+        // Store the relationship
+        clearinghouseToCooler[address(clearinghouse)] = cooler;
+
         // Approve spending of collateral
         vm.prank(wallet_);
-        gohm.approve(cooler, collateralAmount_);
+        gohm.approve(address(clearinghouse), collateralAmount_);
 
         // Create loan
         vm.prank(wallet_);
         clearinghouse.lendToCooler(Cooler(cooler), collateralAmount_);
-
         _;
+    }
+
+    modifier givenWalletHasApprovedMigratorSpendingCollateral(address wallet_, uint256 amount_) {
+        vm.prank(wallet_);
+        gohm.approve(address(migrator), amount_);
+        _;
+    }
+
+    // ========= ASSERTIONS ========= //
+
+    function _expectRevert_disabled() internal {
+        vm.expectRevert(abi.encodeWithSelector(PolicyEnabler.NotEnabled.selector));
     }
 
     // ========= TESTS ========= //
@@ -106,6 +209,23 @@ contract CoolerV2MigratorForkTest is MonoCoolerBaseTest {
     //  [ ] the total borrowed is the principal + interest + flash fee
     // [ ] it returns the total collateral returned
     // [ ] the total borrowed is the principal + interest
+
+    function test_previewConsolidate_givenDisabled_reverts()
+        public
+        givenWalletHasCollateralToken(USER, 1e18)
+        givenWalletHasLoan(USER, true, 1e18)
+        givenDisabled
+    {
+        // Prepare input data
+        address[] memory coolers = new address[](1);
+        coolers[0] = clearinghouseToCooler[address(clearinghouseUsds)];
+
+        // Expect revert
+        _expectRevert_disabled();
+
+        // Function
+        migrator.previewConsolidate(coolers);
+    }
 
     // consolidate
     // given the contract is disabled
