1b62015123555b79b0c1a9acd696767297203416
Jem <0x0xjem@gmail.com>
2025-05-13 15:48:45 +0400

    Adjust compiler version for existing contracts

--- src/test/lib/quabi/Quabi.sol
+++ src/test/lib/quabi/Quabi.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.8.15;
+pragma solidity >=0.8.15;
 
 import {Vm} from "forge-std/Vm.sol";
 import {console2} from "forge-std/console2.sol";
