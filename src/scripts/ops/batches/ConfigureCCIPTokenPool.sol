96e2c22c4854ce4a4ae9116231ab908e09e2cf04
Jem <0x0xjem@gmail.com>
2025-05-16 15:40:37 +0400

    Add install script

--- src/scripts/ops/batches/ConfigureCCIPTokenPool.sol
+++ src/scripts/ops/batches/ConfigureCCIPTokenPool.sol
@@ -3,6 +3,11 @@ pragma solidity >=0.8.24;
 
 import {OlyBatch} from "src/scripts/ops/OlyBatch.sol";
 
+import {Kernel, Actions} from "src/Kernel.sol";
+import {CCIPMintBurnTokenPool} from "src/policies/bridge/CCIPMintBurnTokenPool.sol";
+
+import {console2} from "forge-std/console2.sol";
+
 /// @title ConfigureCCIPTokenPool
 /// @notice Multi-sig batch to configure the CCIP bridge
 ///         This scripts is designed to define the desired configuration,
@@ -16,5 +21,29 @@ contract ConfigureCCIPTokenPool is OlyBatch {
     // chain -> inbound rate limiter config
     // chain -> token address
 
-    function configure(string calldata chain_) external {}
+    address public kernel;
+    CCIPMintBurnTokenPool public tokenPool;
+
+    function loadEnv() internal override {
+        // Load contract addresses from the environment file
+        kernel = envAddress("current", "olympus.Kernel");
+        tokenPool = CCIPMintBurnTokenPool(envAddress("current", "olympus.policies.CCIPMintBurnTokenPool"));
+    }
+
+    function install(bool send_) external isDaoBatch(send_) {
+        // Install the CCIPMintBurnTokenPool policy
+        console2.log("Installing CCIPMintBurnTokenPool policy");
+        addToBatch(
+            kernel,
+            abi.encodeWithSelector(Kernel.executeAction.selector, Actions.ActivatePolicy, address(tokenPool))
+        );
+
+        console2.log("Completed");
+
+        // Next steps:
+        // - Configure the token pool
+        // - Enable the token pool
+    }
+
+    function configure(string calldata chain_, bool send_) external isDaoBatch(send_) {}
 }
