ff2f9609175ef0ba2d29df3e3a23f0a413c6021d
Jem <0x0xjem@gmail.com>
2025-05-19 11:16:58 +0400

    Add Cooler Composites to MS batch script

--- src/scripts/ops/batches/CoolerV2Periphery.sol
+++ src/scripts/ops/batches/CoolerV2Periphery.sol
@@ -22,27 +22,33 @@ contract CoolerV2Periphery is OlyBatch {
 
         // Periphery
         coolerV2Migrator = envAddress("current", "olympus.periphery.CoolerV2Migrator");
+        coolerV2Composites = envAddress("current", "olympus.periphery.CoolerV2Composites");
     }
 
     // Entry point for the batch #1
     function run(bool send_) external isDaoBatch(send_) {
         // Validate addresses
         require(coolerV2Migrator != address(0), "Cooler V2 migrator address is not set");
+        require(coolerV2Composites != address(0), "Cooler V2 composites address is not set");
 
         // 1. Activate the periphery contracts
-        // These won't work until the Cooler V2 policies are enabled by OCG
+        // These won't work until the Cooler V2 policies are installed by the kernel executor
         // They must be enabled here as the DAO MS is the owner
         // Pre-requisites:
         // - Deployment of Cooler V2 contracts
         // - Installation of Cooler V2 contracts (CoolerV2.sol)
         // - Deployment of Cooler V2 periphery contracts
-        console2.log("Activating periphery contracts");
+        console2.log("Enabling Cooler V2 Migrator");
         addToBatch(
             coolerV2Migrator,
             abi.encodeWithSelector(IEnabler.enable.selector, abi.encode(""))
         );
 
-        // TODO add composites here if IEnabler is added
+        console2.log("Enabling Cooler V2 Composites");
+        addToBatch(
+            coolerV2Composites,
+            abi.encodeWithSelector(IEnabler.enable.selector, abi.encode(""))
+        );
 
         console2.log("Batch completed");
     }
