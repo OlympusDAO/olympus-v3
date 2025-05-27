70c73953433374b1317d18f86611656cb444230a
Jem <0x0xjem@gmail.com>
2025-05-27 13:34:51 +0400

    Add string and number parsing to WithEnvironment

--- src/scripts/WithEnvironment.s.sol
+++ src/scripts/WithEnvironment.s.sol
@@ -19,6 +19,8 @@ abstract contract WithEnvironment is Script {
         env = vm.readFile("./src/scripts/env.json");
     }
 
+    // ===== ADDRESSES ===== //
+
     /// @notice Get address from environment file
     ///
     /// @param  chain_  The chain to look up in the environment file
@@ -75,5 +77,89 @@ abstract contract WithEnvironment is Script {
     /// @return address The address from the environment file
     function _envAddressNotZero(string memory key_) internal view returns (address) {
         return _envAddressNotZero(chain, key_);
+    }
+
+    // ===== STRINGS ===== //
+
+    /// @notice Get string from environment file
+    ///
+    /// @param  chain_  The chain to look up in the environment file
+    /// @param  key_    The key to look up in the environment file
+    /// @return string The string from the environment file, or the empty string
+    function _envString(
+        string memory chain_,
+        string memory key_
+    ) internal view returns (string memory) {
+        console2.log("  Checking in env.json for", key_, "on", chain_);
+        string memory fullKey = string.concat(".current.", chain_, ".", key_);
+        string memory str;
+        bool keyExists = vm.keyExists(env, fullKey);
+
+        if (keyExists) {
+            str = env.readString(fullKey);
+            console2.log("    %s: %s (from env.json)", key_, str);
+        } else {
+            console2.log("    %s: *** NOT FOUND ***", key_);
+        }
+
+        return str;
+    }
+
+    function _envString(string memory key_) internal view returns (string memory) {
+        return _envString(chain, key_);
+    }
+
+    function _envStringNotEmpty(
+        string memory chain_,
+        string memory key_
+    ) internal view returns (string memory) {
+        string memory str = _envString(chain_, key_);
+        // solhint-disable-next-line custom-errors
+        require(
+            bytes(str).length > 0,
+            string.concat("WithEnvironment: key '", key_, "' has empty string")
+        );
+
+        return str;
+    }
+
+    function _envStringNotEmpty(string memory key_) internal view returns (string memory) {
+        return _envStringNotEmpty(chain, key_);
+    }
+
+    // ===== NUMBERS ===== //
+
+    function _envUint(string memory chain_, string memory key_) internal view returns (uint256) {
+        console2.log("  Checking in env.json for", key_, "on", chain_);
+        string memory fullKey = string.concat(".current.", chain_, ".", key_);
+        uint256 num;
+        bool keyExists = vm.keyExists(env, fullKey);
+
+        if (keyExists) {
+            num = env.readUint(fullKey);
+            console2.log("    %s: %s (from env.json)", key_, num);
+        } else {
+            console2.log("    %s: *** NOT FOUND ***", key_);
+        }
+
+        return num;
+    }
+
+    function _envUint(string memory key_) internal view returns (uint256) {
+        return _envUint(chain, key_);
+    }
+
+    function _envUintNotZero(
+        string memory chain_,
+        string memory key_
+    ) internal view returns (uint256) {
+        uint256 num = _envUint(chain_, key_);
+        // solhint-disable-next-line custom-errors
+        require(num > 0, string.concat("WithEnvironment: key '", key_, "' has zero value"));
+        return num;
+    }
+
+    function _envUintNotZero(string memory key_) internal view returns (uint256) {
+        return _envUintNotZero(chain, key_);
     }
 }
