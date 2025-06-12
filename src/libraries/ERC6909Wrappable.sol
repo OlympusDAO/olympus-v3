a8640c8989b4254884642df0bebe2f0c1f90e3a3
Jem <0x0xjem@gmail.com>
2025-06-12 16:22:50 +0400

    DepositManager: implement token supply extension in ERC6909Wrappable

--- src/libraries/ERC6909Wrappable.sol
+++ src/libraries/ERC6909Wrappable.sol
@@ -2,21 +2,27 @@
 pragma solidity >=0.8.20;
 
 // Interfaces
-import {IERC165} from "@openzeppelin-5.3.0/interfaces/IERC165.sol";
 import {IERC20BurnableMintable} from "src/interfaces/IERC20BurnableMintable.sol";
+import {IERC165} from "@openzeppelin-5.3.0/interfaces/IERC165.sol";
+import {IERC6909Metadata, IERC6909TokenSupply} from "@openzeppelin-5.3.0/interfaces/draft-IERC6909.sol";
 import {IERC6909Wrappable} from "src/interfaces/IERC6909Wrappable.sol";
 
 // Libraries
+import {ERC6909} from "@openzeppelin-5.3.0/token/ERC6909/draft-ERC6909.sol";
 import {ERC6909Metadata} from "@openzeppelin-5.3.0/token/ERC6909/extensions/draft-ERC6909Metadata.sol";
 import {ClonesWithImmutableArgs} from "@clones-with-immutable-args-1.1.2/ClonesWithImmutableArgs.sol";
 
 /// @title ERC6909Wrappable
 /// @notice This abstract contract extends ERC6909 to allow for wrapping and unwrapping of the token to an ERC20 token.
-abstract contract ERC6909Wrappable is ERC6909Metadata, IERC6909Wrappable {
+///         It extends the ERC6909Metadata contract, and additionally implements the IERC6909TokenSupply interface.
+abstract contract ERC6909Wrappable is ERC6909Metadata, IERC6909Wrappable, IERC6909TokenSupply {
     using ClonesWithImmutableArgs for address;
 
     /// @notice The address of the implementation of the ERC20 contract
     address private immutable _ERC20_IMPLEMENTATION;
+
+    /// @notice The total supply of each token
+    mapping(uint256 id => uint256) private _totalSupplies;
 
     /// @notice Additional metadata for each token
     mapping(uint256 => bytes) internal _tokenMetadataAdditional;
@@ -92,6 +98,34 @@ abstract contract ERC6909Wrappable is ERC6909Metadata, IERC6909Wrappable {
         }
     }
 
+    // ========== TOTAL SUPPLY EXTENSION ========== //
+
+    /// @inheritdoc IERC6909TokenSupply
+    function totalSupply(uint256 tokenId_) public view virtual override returns (uint256) {
+        return _totalSupplies[tokenId_];
+    }
+
+    /// @dev Copied from draft-ERC6909TokenSupply.sol
+    function _update(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount
+    ) internal virtual override {
+        // Calls ERC6909Metadata._update()
+        super._update(from, to, id, amount);
+
+        if (from == address(0)) {
+            _totalSupplies[id] += amount;
+        }
+        if (to == address(0)) {
+            unchecked {
+                // amount <= _balances[from][id] <= _totalSupplies[id]
+                _totalSupplies[id] -= amount;
+            }
+        }
+    }
+
     // ========== WRAP/UNWRAP FUNCTIONS ========== //
 
     /// @dev Returns the address of the wrapped ERC20 token for a given token ID, or creates a new one if it does not exist
@@ -144,5 +178,17 @@ abstract contract ERC6909Wrappable is ERC6909Metadata, IERC6909Wrappable {
     /// @inheritdoc IERC6909Wrappable
     function isValidTokenId(uint256 tokenId_) public view returns (bool) {
         return decimals(tokenId_) > 0;
+    }
+
+    // ========== ERC165 ========== //
+
+    function supportsInterface(
+        bytes4 interfaceId_
+    ) public view virtual override(ERC6909, IERC165) returns (bool) {
+        return
+            interfaceId_ == type(IERC6909Wrappable).interfaceId ||
+            interfaceId_ == type(IERC6909Metadata).interfaceId ||
+            interfaceId_ == type(IERC6909TokenSupply).interfaceId ||
+            super.supportsInterface(interfaceId_);
     }
 }
