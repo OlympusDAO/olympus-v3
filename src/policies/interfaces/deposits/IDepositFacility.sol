5448908ace75633a945b4eaf7e4b361e6b4c4f56
Jem <0x0xjem@gmail.com>
2025-07-18 10:15:00 +1000

    Consistent pragma for interfaces

--- src/policies/interfaces/deposits/IDepositFacility.sol
+++ src/policies/interfaces/deposits/IDepositFacility.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
 // Interfaces
 import {IERC20} from "src/interfaces/IERC20.sol";
--- src/policies/interfaces/deposits/IDepositManager.sol
+++ src/policies/interfaces/deposits/IDepositManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.15;
+pragma solidity >=0.8.0;
 
 import {IERC20} from "src/interfaces/IERC20.sol";
 import {IERC4626} from "src/interfaces/IERC4626.sol";
