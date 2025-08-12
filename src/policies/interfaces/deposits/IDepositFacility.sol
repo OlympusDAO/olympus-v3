3453f6384d9126341f05c983095a15c7cdd142ee
Jem <0x0xjem@gmail.com>
2025-08-12 17:26:23 +0400

    Docs cleanup

--- src/policies/interfaces/deposits/IDepositFacility.sol
+++ src/policies/interfaces/deposits/IDepositFacility.sol
@@ -79,7 +79,7 @@ interface IDepositFacility {
     ///
     /// @param depositToken_    The deposit token committed
     /// @param depositPeriod_   The deposit period in months
-    /// @param amount_          The amount to cancel the committed funds by
+    /// @param amount_          The amount to reduce the commitment by
     function handleCommitCancel(
         IERC20 depositToken_,
         uint8 depositPeriod_,
@@ -120,7 +120,7 @@ interface IDepositFacility {
     /// @param depositPeriod_   The deposit period in months
     /// @param amount_          The amount being repaid
     /// @param payer_           The address making the repayment
-    /// @return actualAmount    The amount of tokens borrowed
+    /// @return actualAmount    The amount of tokens repaid
     function handleLoanRepay(
         IERC20 depositToken_,
         uint8 depositPeriod_,
@@ -208,25 +208,28 @@ interface IDepositFacility {
 
     // ========== BALANCE QUERIES ========== //
 
-    /// @notice Get the available deposit balance for a specific token. This excludes any committed funds.
+    /// @notice Get the available deposit balance for a specific token.
+    ///         This excludes any committed funds.
     ///
     /// @param depositToken_ The deposit token to query
     /// @return balance     The available deposit balance
     function getAvailableDeposits(IERC20 depositToken_) external view returns (uint256 balance);
 
     /// @notice Get the committed deposits for a specific token.
-    ///         The committed deposits represents the amount of the deposit token that this contract ensures will be available for all of the operators to use.
+    ///         The committed deposits value represents the amount of the deposit token
+    ///         that this contract ensures will be available for all operators to use.
     ///
     /// @param depositToken_    The deposit token to query
     /// @return committed       The total committed deposits
     function getCommittedDeposits(IERC20 depositToken_) external view returns (uint256 committed);
 
     /// @notice Get the committed deposits for a specific token and operator.
-    ///         The committed deposits represents the amount of the deposit token that this contract ensures will be available for the operator to use.
+    ///         The committed deposits value represents the amount of the deposit token
+    ///         that this contract ensures will be available for the specific operator to use.
     ///
-    /// @param depositToken_ The deposit token to query
-    /// @param operator_     The operator
-    /// @return committed   The total committed deposits
+    /// @param depositToken_    The deposit token to query
+    /// @param operator_        The operator
+    /// @return committed       The committed deposits for the operator
     function getCommittedDeposits(
         IERC20 depositToken_,
         address operator_
