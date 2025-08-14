f4c6a7e178209bfdb08429885e1c4dd6b4e0efc9
Jem <0x0xjem@gmail.com>
2025-08-14 16:55:04 +0400

    fix: cross-port from 25008beff73b50b9755b15634ba66fba33a75e7d ordering of updates to committed deposits and storing actual amounts

--- src/policies/deposits/BaseDepositFacility.sol
+++ src/policies/deposits/BaseDepositFacility.sol
@@ -197,14 +197,6 @@ abstract contract BaseDepositFacility is Policy, PolicyEnabler, IDepositFacility
         if (amount_ > operatorCommitments)
             revert DepositFacility_InsufficientCommitment(msg.sender, amount_, operatorCommitments);
 
-        // Reduce the commitment
-        // The input amount is used here, in order to avoid having residual values
-        // (which the calling operator has no control over)
-        _assetOperatorCommittedDeposits[
-            _getCommittedDepositsKey(depositToken_, msg.sender)
-        ] -= amount_;
-        _assetCommittedDeposits[depositToken_] -= amount_;
-
         // Process the withdrawal through DepositManager
         uint256 actualAmount = DEPOSIT_MANAGER.withdraw(
             IDepositManager.WithdrawParams({
@@ -219,6 +211,12 @@ abstract contract BaseDepositFacility is Policy, PolicyEnabler, IDepositFacility
 
         // Validate that the amount is not zero
         if (actualAmount == 0) revert DepositFacility_ZeroAmount();
+
+        // Reduce the commitment
+        _assetOperatorCommittedDeposits[
+            _getCommittedDepositsKey(depositToken_, msg.sender)
+        ] -= actualAmount;
+        _assetCommittedDeposits[depositToken_] -= actualAmount;
 
         // Emit event
         emit AssetCommitWithdrawn(address(depositToken_), msg.sender, actualAmount);
@@ -241,13 +239,6 @@ abstract contract BaseDepositFacility is Policy, PolicyEnabler, IDepositFacility
         uint256 operatorCommitments = getCommittedDeposits(depositToken_, msg.sender);
         if (amount_ > operatorCommitments)
             revert DepositFacility_InsufficientCommitment(msg.sender, amount_, operatorCommitments);
-
-        // Reduce committed deposits by the amount borrowed
-        // This is done prior to any external contract calls to mitigate re-entrancy
-        _assetOperatorCommittedDeposits[
-            _getCommittedDepositsKey(depositToken_, msg.sender)
-        ] -= amount_;
-        _assetCommittedDeposits[depositToken_] -= amount_;
 
         // Process the borrowing through DepositManager
         // It will revert if more is being borrowed than available
