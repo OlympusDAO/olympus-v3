19bc3f6e901adc8f514f0d384123c436f958370b
Jem <0x0xjem@gmail.com>
2025-08-14 18:39:32 +0400

    fix: DepositRedemptionVault: swap order of state update

--- src/policies/deposits/BaseDepositFacility.sol
+++ src/policies/deposits/BaseDepositFacility.sol
@@ -184,6 +184,14 @@ abstract contract BaseDepositFacility is Policy, PolicyEnabler, IDepositFacility
         if (amount_ > operatorCommitments)
             revert DepositFacility_InsufficientCommitment(msg.sender, amount_, operatorCommitments);
 
+        // Reduce the commitment
+        // The input amount is used here, in order to avoid having residual values
+        // (which the calling operator has no control over)
+        _assetOperatorCommittedDeposits[
+            _getCommittedDepositsKey(depositToken_, msg.sender)
+        ] -= amount_;
+        _assetCommittedDeposits[depositToken_] -= amount_;
+
         // Process the withdrawal through DepositManager
         uint256 actualAmount = DEPOSIT_MANAGER.withdraw(
             IDepositManager.WithdrawParams({
@@ -198,14 +206,6 @@ abstract contract BaseDepositFacility is Policy, PolicyEnabler, IDepositFacility
 
         // Validate that the amount is not zero
         if (actualAmount == 0) revert DepositFacility_ZeroAmount();
-
-        // Reduce the commitment
-        // The input amount is used here, in order to avoid having residual values
-        // (which the calling operator has no control over)
-        _assetOperatorCommittedDeposits[
-            _getCommittedDepositsKey(depositToken_, msg.sender)
-        ] -= amount_;
-        _assetCommittedDeposits[depositToken_] -= amount_;
 
         // Emit event
         emit AssetCommitWithdrawn(address(depositToken_), msg.sender, actualAmount);
