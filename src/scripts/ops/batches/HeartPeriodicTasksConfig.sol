60e850243de2fd81d551baae60d53b773badc110
Jem <0x0xjem@gmail.com>
2025-08-22 15:50:50 +0500

    Batch script for configuring periodic tasks on Heart

---
+++ src/scripts/ops/batches/HeartPeriodicTasksConfig.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity >=0.8.15;
+
+import {BatchScriptV2} from "src/scripts/ops/lib/BatchScriptV2.sol";
+
+// Interfaces
+import {IPeriodicTaskManager} from "src/bases/interfaces/IPeriodicTaskManager.sol";
+import {IReserveMigrator} from "src/policies/interfaces/IReserveMigrator.sol";
+import {IReserveWrapper} from "src/policies/interfaces/IReserveWrapper.sol";
+import {IOperator} from "src/policies/interfaces/IOperator.sol";
+import {IYieldRepo} from "src/policies/interfaces/IYieldRepo.sol";
+
+import {console2} from "@forge-std-1.9.6/console2.sol";
+
+/// @notice Configures the Heart with periodic tasks for reserve operations
+/// @dev    This is designed for use with Heart v1.7
+contract HeartPeriodicTasksConfig is BatchScriptV2 {
+    /// @notice Configure Heart with periodic tasks in the specified order
+    function configurePeriodicTasks(bool useDaoMS_) external setUpWithChainId(useDaoMS_) {
+        // Load contract addresses
+        address heart = _envAddressNotZero("olympus.policies.OlympusHeart");
+        address reserveMigrator = _envAddressNotZero("olympus.policies.ReserveMigrator");
+        address reserveWrapper = _envAddressNotZero("olympus.policies.ReserveWrapper");
+        address operator = _envAddressNotZero("olympus.policies.Operator");
+        address yieldRepo = _envAddressNotZero("olympus.policies.YieldRepurchaseFacility");
+
+        // Assumes that there are no existing tasks
+        // solhint-disable-next-line gas-custom-errors
+        require(
+            IPeriodicTaskManager(heart).getPeriodicTaskCount() == 0,
+            "Heart already has periodic tasks configured"
+        );
+
+        // Add periodic tasks to Heart in the specified order:
+        // 1. reserveMigrator.migrate();
+        console2.log("Adding ReserveMigrator.migrate() task to Heart");
+        addToBatch(
+            heart,
+            abi.encodeWithSelector(
+                IPeriodicTaskManager.addPeriodicTaskAtIndex.selector,
+                reserveMigrator,
+                IReserveMigrator.migrate.selector,
+                0 // First task
+            )
+        );
+
+        // 2. reserveWrapper.execute();
+        console2.log("Adding ReserveWrapper.execute() task to Heart");
+        addToBatch(
+            heart,
+            abi.encodeWithSelector(IPeriodicTaskManager.addPeriodicTask.selector, reserveWrapper)
+        );
+
+        // 3. operator.operate();
+        console2.log("Adding Operator.operate() task to Heart");
+        addToBatch(
+            heart,
+            abi.encodeWithSelector(
+                IPeriodicTaskManager.addPeriodicTaskAtIndex.selector,
+                operator,
+                IOperator.operate.selector,
+                2 // Third task
+            )
+        );
+
+        // 4. yieldRepo.endEpoch();
+        console2.log("Adding YieldRepo.endEpoch() task to Heart");
+        addToBatch(
+            heart,
+            abi.encodeWithSelector(
+                IPeriodicTaskManager.addPeriodicTaskAtIndex.selector,
+                yieldRepo,
+                IYieldRepo.endEpoch.selector,
+                3 // Fourth task
+            )
+        );
+
+        // Run
+        proposeBatch();
+    }
+}
