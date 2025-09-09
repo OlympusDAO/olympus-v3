cb0a2856de8f72a4303908d8d7890dcd78f174b8
Jem <0x0xjem@gmail.com>
2025-09-09 14:48:16 +0400

    chore: remove unused imports

--- src/policies/interfaces/IHeart_v1_6.sol
+++ src/policies/interfaces/IHeart_v1_6.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.0;
 
-import {ERC20} from "@solmate-6.2.0/tokens/ERC20.sol";
-
 /// @dev Interface for the Heart policy as of v1.6
 interface IHeart {
     // =========  EVENTS ========= //
