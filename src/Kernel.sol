0771f6bf17bc615e2427ef781ad2e00333eba70b
Jem <0x0xjem@gmail.com>
2025-09-25 15:39:14 +0400

    Disable solhint and forge lint warnings in production contracts/interfaces

--- src/Kernel.sol
+++ src/Kernel.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
+// solhint-disable one-contract-per-file
 pragma solidity >=0.8.15;
 
 //     ███████    █████       █████ █████ ██████   ██████ ███████████  █████  █████  █████████
