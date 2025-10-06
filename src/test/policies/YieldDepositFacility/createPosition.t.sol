22af257ac5a6c3d2ef04b583b2b9cb250c717765
Jem <0x0xjem@gmail.com>
2025-10-06 14:47:58 +0400

    Revert "Remove YieldDepositFacility and tests"
    This reverts commit 797fac5ef72aaa601f68a25c07e1998d4488ce06.

---
+++ src/test/policies/YieldDepositFacility/createPosition.t.sol
@@ -0,0 +1,304 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.8.20;
+
+import {YieldDepositFacilityTest} from "./YieldDepositFacilityTest.sol";
+import {IERC20} from "src/interfaces/IERC20.sol";
+import {IERC4626} from "src/interfaces/IERC4626.sol";
+import {MockERC20} from "@solmate-6.2.0/test/utils/mocks/MockERC20.sol";
+import {IYieldDepositFacility} from "src/policies/interfaces/deposits/IYieldDepositFacility.sol";
+
+contract YieldDepositFacilityCreatePositionTest is YieldDepositFacilityTest {
+    event CreatedDeposit(
+        address indexed asset,
+        address indexed depositor,
+        uint256 indexed positionId,
+        uint8 periodMonths,
+        uint256 depositAmount
+    );
+
+    // given the contract is inactive
+    //  [X] it reverts
+
+    function test_contractInactive_reverts() public {
+        // Expect revert
+        _expectRevertNotEnabled();
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.createPosition(
+            IYieldDepositFacility.CreatePositionParams({
+                asset: iReserveToken,
+                periodMonths: PERIOD_MONTHS,
+                amount: RESERVE_TOKEN_AMOUNT,
+                wrapPosition: false,
+                wrapReceipt: false
+            })
+        );
+    }
+
+    // given the asset is not yield-bearing
+    //  [X] it reverts
+
+    function test_givenAssetIsNotYieldBearing_reverts() public givenLocallyActive {
+        // Create a new asset
+        MockERC20 newAsset = new MockERC20("New Asset", "NEW", 18);
+        vm.prank(admin);
+        depositManager.addAsset(
+            IERC20(address(newAsset)),
+            IERC4626(address(0)),
+            type(uint256).max,
+            0
+        );
+        IERC20 iNewAsset = IERC20(address(newAsset));
+
+        // Expect revert
+        _expectRevertInvalidToken(iNewAsset, PERIOD_MONTHS);
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.createPosition(
+            IYieldDepositFacility.CreatePositionParams({
+                asset: iNewAsset,
+                periodMonths: PERIOD_MONTHS,
+                amount: RESERVE_TOKEN_AMOUNT,
+                wrapPosition: false,
+                wrapReceipt: false
+            })
+        );
+    }
+
+    // when the recipient has not approved DepositManager to spend the reserve tokens
+    //  [X] it reverts
+
+    function test_spendingNotApproved_reverts()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+    {
+        // Expect revert
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+
+        // Call function
+        _createYieldDepositPosition(recipient, RESERVE_TOKEN_AMOUNT);
+    }
+
+    // given the deposit asset has 6 decimals
+    //  [X] the amount of receipt tokens minted is correct
+
+    function test_reserveTokenHasSmallerDecimals()
+        public
+        givenReserveTokenHasDecimals(6)
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, 10e6)
+        givenReserveTokenSpendingIsApproved(recipient, address(depositManager), 10e6)
+    {
+        // Expect event
+        vm.expectEmit(true, true, true, true);
+        emit CreatedDeposit(address(reserveToken), recipient, 0, PERIOD_MONTHS, 10e6);
+
+        // Call function
+        (
+            uint256 actualPositionId,
+            uint256 actualReceiptTokenId,
+            uint256 actualAmount
+        ) = _createYieldDepositPosition(recipient, 10e6);
+
+        // Assert that the position ID is 0
+        assertEq(actualPositionId, 0, "positionId");
+
+        // Assert that the receipt token ID is correct
+        assertEq(actualReceiptTokenId, _receiptTokenId, "_receiptTokenId");
+
+        // Assert that the conversion price is correct
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).conversionPrice,
+            type(uint256).max,
+            "conversionPrice"
+        );
+
+        // Assert that the actual amount is correct
+        assertApproxEqAbs(actualAmount, 10e6, 1, "actualAmount");
+
+        // Assert that the reserve token was transferred from the recipient
+        _assertReserveTokenBalance(0);
+
+        // Assert that the DepositManager token was minted to the recipient
+        _assertReceiptTokenBalance(recipient, actualAmount, false);
+
+        // Assert that the recipient has a DEPOS position
+        uint256[] memory positionIds = convertibleDepositPositions.getUserPositionIds(recipient);
+        assertEq(positionIds.length, 1, "positionIds.length");
+        assertEq(positionIds[0], 0, "positionIds[0]");
+    }
+
+    // [X] it mints the receipt tokens to account_
+    // [X] it creates a new position in the DEPOS module
+    // [X] the position does not have a conversion price
+    // [X] it returns the position ID
+    // [X] it emits a CreatedDeposit event
+    // [X] it sets the last yield conversion rate
+
+    function test_success()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+        givenReserveTokenSpendingIsApproved(
+            recipient,
+            address(depositManager),
+            RESERVE_TOKEN_AMOUNT
+        )
+    {
+        // Expect event
+        vm.expectEmit(true, true, true, false);
+        emit CreatedDeposit(
+            address(reserveToken),
+            recipient,
+            0,
+            PERIOD_MONTHS,
+            RESERVE_TOKEN_AMOUNT
+        );
+
+        // Call function
+        (
+            uint256 actualPositionId,
+            uint256 actualReceiptTokenId,
+            uint256 actualAmount
+        ) = _createYieldDepositPosition(recipient, RESERVE_TOKEN_AMOUNT);
+
+        // Assert that the position ID is 0
+        assertEq(actualPositionId, 0, "positionId");
+
+        // Assert that the receipt token ID is correct
+        assertEq(actualReceiptTokenId, _receiptTokenId, "_receiptTokenId");
+
+        // Assert that the conversion price is correct
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).conversionPrice,
+            type(uint256).max,
+            "conversionPrice"
+        );
+
+        // Assert that the actual amount is correct
+        assertApproxEqAbs(actualAmount, RESERVE_TOKEN_AMOUNT, 1, "actualAmount");
+
+        // Assert that the position is not convertible
+        assertEq(
+            convertibleDepositPositions.isConvertible(actualPositionId),
+            false,
+            "isConvertible"
+        );
+
+        // Assert that the operator is the YDF
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).operator,
+            address(yieldDepositFacility),
+            "operator"
+        );
+
+        // Assert that the remaining deposit matches the actual amount
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).remainingDeposit,
+            actualAmount,
+            "remainingDeposit"
+        );
+
+        // Assert that the reserve token was transferred from the recipient
+        _assertReserveTokenBalance(0);
+
+        // Assert that the receipt token token was minted to the recipient
+        _assertReceiptTokenBalance(recipient, actualAmount, false);
+
+        // Assert that the recipient has a DEPOS position
+        uint256[] memory positionIds = convertibleDepositPositions.getUserPositionIds(recipient);
+        assertEq(positionIds.length, 1, "positionIds.length");
+        assertEq(positionIds[0], 0, "positionIds[0]");
+
+        // Assert that the last yield claim timestamp is set
+        assertEq(
+            yieldDepositFacility.positionLastYieldClaimTimestamp(actualPositionId),
+            block.timestamp,
+            "positionLastYieldClaimTimestamp"
+        );
+    }
+
+    function test_success_fuzz(
+        uint256 amount_
+    )
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+        givenReserveTokenSpendingIsApproved(
+            recipient,
+            address(depositManager),
+            RESERVE_TOKEN_AMOUNT
+        )
+    {
+        amount_ = bound(amount_, 1e18, RESERVE_TOKEN_AMOUNT);
+
+        // Expect event
+        vm.expectEmit(true, true, true, false);
+        emit CreatedDeposit(address(reserveToken), recipient, 0, PERIOD_MONTHS, amount_);
+
+        // Call function
+        (
+            uint256 actualPositionId,
+            uint256 actualReceiptTokenId,
+            uint256 actualAmount
+        ) = _createYieldDepositPosition(recipient, amount_);
+
+        // Assert that the position ID is 0
+        assertEq(actualPositionId, 0, "positionId");
+
+        // Assert that the receipt token ID is correct
+        assertEq(actualReceiptTokenId, _receiptTokenId, "_receiptTokenId");
+
+        // Assert that the conversion price is correct
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).conversionPrice,
+            type(uint256).max,
+            "conversionPrice"
+        );
+
+        // Assert that the actual amount is correct
+        assertApproxEqAbs(actualAmount, amount_, 1, "actualAmount");
+
+        // Assert that the position is not convertible
+        assertEq(
+            convertibleDepositPositions.isConvertible(actualPositionId),
+            false,
+            "isConvertible"
+        );
+
+        // Assert that the operator is the YDF
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).operator,
+            address(yieldDepositFacility),
+            "operator"
+        );
+
+        // Assert that the remaining deposit matches the actual amount
+        assertEq(
+            convertibleDepositPositions.getPosition(actualPositionId).remainingDeposit,
+            actualAmount,
+            "remainingDeposit"
+        );
+
+        // Assert that the reserve token was transferred from the recipient
+        _assertReserveTokenBalance(RESERVE_TOKEN_AMOUNT - amount_);
+
+        // Assert that the receipt token token was minted to the recipient
+        _assertReceiptTokenBalance(recipient, actualAmount, false);
+
+        // Assert that the recipient has a DEPOS position
+        uint256[] memory positionIds = convertibleDepositPositions.getUserPositionIds(recipient);
+        assertEq(positionIds.length, 1, "positionIds.length");
+        assertEq(positionIds[0], 0, "positionIds[0]");
+
+        // Assert that the last yield claim timestamp is set
+        assertEq(
+            yieldDepositFacility.positionLastYieldClaimTimestamp(actualPositionId),
+            block.timestamp,
+            "positionLastYieldClaimTimestamp"
+        );
+    }
+}
---
+++ src/test/policies/YieldDepositFacility/deposit.t.sol
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.20;
+
+import {YieldDepositFacilityTest} from "./YieldDepositFacilityTest.sol";
+import {IERC20} from "src/interfaces/IERC20.sol";
+import {IERC4626} from "src/interfaces/IERC4626.sol";
+import {MockERC20} from "@solmate-6.2.0/test/utils/mocks/MockERC20.sol";
+
+import {console2} from "@forge-std-1.9.6/console2.sol";
+
+contract YieldDepositFacilityDepositTest is YieldDepositFacilityTest {
+    // given the contract is disabled
+    //  [X] it reverts
+
+    function test_givenContractIsDisabled_reverts() public {
+        // Expect revert
+        _expectRevertNotEnabled();
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.deposit(iReserveToken, PERIOD_MONTHS, RESERVE_TOKEN_AMOUNT, false);
+    }
+
+    // given the deposit is not configured
+    //  [X] it reverts
+
+    function test_givenDepositIsNotConfigured_reverts() public givenLocallyActive {
+        // Expect revert
+        _expectRevertDepositManagerInvalidConfiguration(iReserveToken, PERIOD_MONTHS + 1);
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.deposit(iReserveToken, PERIOD_MONTHS + 1, RESERVE_TOKEN_AMOUNT, false);
+    }
+
+    // given the asset is not yield-bearing
+    //  [X] it reverts
+
+    function test_givenAssetIsNotYieldBearing_reverts() public givenLocallyActive {
+        // Create a new asset
+        MockERC20 newAsset = new MockERC20("New Asset", "NEW", 18);
+        vm.prank(admin);
+        depositManager.addAsset(
+            IERC20(address(newAsset)),
+            IERC4626(address(0)),
+            type(uint256).max,
+            0
+        );
+        IERC20 iNewAsset = IERC20(address(newAsset));
+
+        // Expect revert
+        _expectRevertInvalidToken(iNewAsset, PERIOD_MONTHS);
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.deposit(iNewAsset, PERIOD_MONTHS, RESERVE_TOKEN_AMOUNT, false);
+    }
+
+    // given the caller has not approved the deposit manager to spend the asset
+    //  [X] it reverts
+
+    function test_spendingNotApproved_reverts()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+    {
+        // Expect revert
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.deposit(iReserveToken, PERIOD_MONTHS, RESERVE_TOKEN_AMOUNT, false);
+    }
+
+    // given the caller does not have the required asset balance
+    //  [X] it reverts
+
+    function test_insufficientBalance_reverts()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+        givenReserveTokenSpendingIsApproved(
+            recipient,
+            address(depositManager),
+            RESERVE_TOKEN_AMOUNT + 1
+        )
+    {
+        // Expect revert
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+
+        // Call function
+        vm.prank(recipient);
+        yieldDepositFacility.deposit(iReserveToken, PERIOD_MONTHS, RESERVE_TOKEN_AMOUNT + 1, false);
+    }
+
+    // when wrap receipt is true
+    //  [X] it transfers the asset from the caller
+    //  [X] it transfers the wrapped receipt token to the caller
+    //  [X] it returns the receipt token id
+    //  [X] it returns the actual deposit amount
+    //  [X] it does not create a position
+
+    function test_whenWrapReceiptIsTrue()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+        givenReserveTokenSpendingIsApproved(
+            recipient,
+            address(depositManager),
+            RESERVE_TOKEN_AMOUNT
+        )
+    {
+        uint256 expectedReceiptTokenId = depositManager.getReceiptTokenId(
+            iReserveToken,
+            PERIOD_MONTHS,
+            address(yieldDepositFacility)
+        );
+
+        // Call function
+        vm.prank(recipient);
+        (uint256 receiptTokenId, uint256 actualDepositAmount) = yieldDepositFacility.deposit(
+            iReserveToken,
+            PERIOD_MONTHS,
+            RESERVE_TOKEN_AMOUNT,
+            true
+        );
+
+        // Assert that the receipt token id is correct
+        assertEq(receiptTokenId, expectedReceiptTokenId, "receiptTokenId");
+
+        // Assert that the reserve token was transferred from the recipient
+        assertEq(reserveToken.balanceOf(recipient), 0, "reserveToken.balanceOf(recipient)");
+
+        // Assert that the wrapped receipt token amount is correct
+        assertApproxEqAbs(actualDepositAmount, RESERVE_TOKEN_AMOUNT, 1, "actualDepositAmount");
+
+        // Assert that the wrapped receipt token was minted to the recipient
+        _assertReceiptTokenBalance(recipient, actualDepositAmount, true);
+
+        // Assert that the recipient does not have a DEPOS position
+        uint256[] memory positionIds = convertibleDepositPositions.getUserPositionIds(recipient);
+        assertEq(positionIds.length, 0, "positionIds.length");
+    }
+
+    // [X] it transfers the asset from the caller
+    // [X] it transfers the receipt token to the caller
+    // [X] it returns the receipt token id
+    // [X] it returns the actual deposit amount
+    // [X] it does not create a position
+
+    /// forge-config: default.isolate = true
+    function test_success()
+        public
+        givenLocallyActive
+        givenAddressHasReserveToken(recipient, RESERVE_TOKEN_AMOUNT)
+        givenReserveTokenSpendingIsApproved(
+            recipient,
+            address(depositManager),
+            RESERVE_TOKEN_AMOUNT
+        )
+    {
+        uint256 expectedReceiptTokenId = depositManager.getReceiptTokenId(
+            iReserveToken,
+            PERIOD_MONTHS,
+            address(yieldDepositFacility)
+        );
+
+        // Start gas snapshot
+        vm.startSnapshotGas("deposit");
+
+        // Call function
+        vm.prank(recipient);
+        (uint256 receiptTokenId, uint256 actualDepositAmount) = yieldDepositFacility.deposit(
+            iReserveToken,
+            PERIOD_MONTHS,
+            RESERVE_TOKEN_AMOUNT,
+            false
+        );
+
+        // Stop gas snapshot
+        uint256 gasUsed = vm.stopSnapshotGas();
+        console2.log("Gas used", gasUsed);
+
+        // Assert that the receipt token id is correct
+        assertEq(receiptTokenId, expectedReceiptTokenId, "receiptTokenId");
+
+        // Assert that the reserve token was transferred from the recipient
+        assertEq(reserveToken.balanceOf(recipient), 0, "reserveToken.balanceOf(recipient)");
+
+        // Assert that the receipt token amount is correct
+        assertApproxEqAbs(actualDepositAmount, RESERVE_TOKEN_AMOUNT, 1, "actualDepositAmount");
+
+        // Assert that the receipt token was minted to the recipient
+        _assertReceiptTokenBalance(recipient, actualDepositAmount, false);
+
+        // Assert that the recipient does not have a DEPOS position
+        uint256[] memory positionIds = convertibleDepositPositions.getUserPositionIds(recipient);
+        assertEq(positionIds.length, 0, "positionIds.length");
+    }
+}
