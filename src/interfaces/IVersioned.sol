157dda5fa204db9e99b0fb1e383a6adf512ad31b
Jem <0x0xjem@gmail.com>
2025-10-17 16:04:51 +0400

    Ignore linting warnings

--- src/interfaces/IVersioned.sol
+++ src/interfaces/IVersioned.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+/// forge-lint: disable-start(mixed-case-function)
 pragma solidity ^0.8.15;
 
 /// @title  IVersioned
@@ -10,3 +11,4 @@ interface IVersioned {
     /// @return minor The minor version of the contract
     function VERSION() external view returns (uint8 major, uint8 minor);
 }
+/// forge-lint: disable-end(mixed-case-function)
