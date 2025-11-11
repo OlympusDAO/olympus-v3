760ac797db26cfd5bdfb56e260d624badb3efe4f
Jem <0x0xjem@gmail.com>
2025-11-11 12:09:35 +0400

    Add test for Convertible Deposits OCG proposal

---
+++ src/test/proposals/ConvertibleDepositProposal.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {ProposalTest} from "./ProposalTest.sol";
+
+// ConvertibleDepositProposal imports
+import {ConvertibleDepositProposal} from "src/proposals/ConvertibleDepositProposal.sol";
+
+contract ConvertibleDepositProposalTest is ProposalTest {
+    function setUp() public virtual {
+        // Mainnet fork at a fixed block prior to proposal execution to ensure deterministic state
+        vm.createSelectFork(_RPC_ALIAS, 23774671);
+
+        // Deploy proposal under test
+        ConvertibleDepositProposal proposal = new ConvertibleDepositProposal();
+
+        // Set to true once the proposal has been submitted on-chain to enforce calldata matching
+        hasBeenSubmitted = false;
+
+        // Simulate the proposal
+        _setupSuite(address(proposal));
+        _simulateProposal();
+    }
+}
