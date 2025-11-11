eb04899e00700c7d6a750b59860832f3ebd6ed56
Jem <0x0xjem@gmail.com>
2025-11-11 13:47:37 +0400

    Split Cooler composites/migrator into DAO MS-specific script

--- src/scripts/emergency/CoolerV2.sol
+++ src/scripts/emergency/CoolerV2.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.15;
 import {BatchScriptV2} from "src/scripts/ops/lib/BatchScriptV2.sol";
 import {IEmergencyBatch} from "src/scripts/emergency/IEmergencyBatch.sol";
 import {IMonoCooler} from "src/policies/interfaces/cooler/IMonoCooler.sol";
-import {IEnabler} from "src/periphery/interfaces/IEnabler.sol";
 import {console2} from "@forge-std-1.9.6/console2.sol";
 
 contract CoolerV2 is BatchScriptV2, IEmergencyBatch {
@@ -17,9 +16,6 @@ contract CoolerV2 is BatchScriptV2, IEmergencyBatch {
     /// @dev    Shuts down:
     ///         - Pauses borrows on CoolerV2
     ///         - Pauses liquidations on CoolerV2
-    ///         - Disables CoolerComposites
-    ///         - Disables CoolerV2Migrator
-    ///         Uses Emergency MS for execution (for CoolerV2), DAO MS for composites/migrator
     function run(
         bool signOnly_,
         string memory argsFilePath_,
@@ -49,16 +45,6 @@ contract CoolerV2 is BatchScriptV2, IEmergencyBatch {
             coolerV2Address,
             abi.encodeWithSelector(IMonoCooler.setLiquidationsPaused.selector, true)
         );
-
-        // Disable CoolerComposites
-        address compositesAddress = _envAddressNotZero("olympus.periphery.CoolerComposites");
-        console2.log("  Disabling CoolerComposites");
-        addToBatch(compositesAddress, abi.encodeWithSelector(IEnabler.disable.selector, ""));
-
-        // Disable CoolerV2Migrator
-        address migratorAddress = _envAddressNotZero("olympus.periphery.CoolerV2Migrator");
-        console2.log("  Disabling CoolerV2Migrator");
-        addToBatch(migratorAddress, abi.encodeWithSelector(IEnabler.disable.selector, ""));
 
         // Run
         proposeBatch();
