9e92b07e6af8a6b5ee31d18c5f9ac565faa47f07
Jem <0x0xjem@gmail.com>
2025-12-24 16:24:40 +0500

    chore: relax compiler version

--- src/test/policies/EmissionManager.t.sol
+++ src/test/policies/EmissionManager.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 /// forge-lint: disable-start(mixed-case-variable, mixed-case-function, unwrapped-modifier-logic)
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {UserFactory} from "src/test/lib/UserFactory.sol";
