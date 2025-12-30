e6887cbe36fcd5f42b333f7287961a48509862d9
Jem <0x0xjem@gmail.com>
2025-12-30 13:40:53 +0400

    test: shift migrator test to perform actions using the timelock, add unstaking to OHM v2 and burning

--- src/test/MigratorForkTest.t.sol
+++ src/test/MigratorForkTest.t.sol
@@ -5,90 +5,13 @@ import {Test} from "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
 import {IERC20} from "src/interfaces/IERC20.sol";
 import {IgOHM} from "src/interfaces/IgOHM.sol";
+import {IStaking} from "src/interfaces/IStaking.sol";
 import {ERC20} from "@openzeppelin-5.3.0/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin-5.3.0/access/Ownable.sol";
-
-interface OlympusERC20Token {
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    event TWAPEpochChanged(uint256 previousTWAPEpochPeriod, uint256 newTWAPEpochPeriod);
-    event TWAPOracleChanged(address indexed previousTWAPOracle, address indexed newTWAPOracle);
-    event TWAPSourceAdded(address indexed newTWAPSource);
-    event TWAPSourceRemoved(address indexed removedTWAPSource);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-
-    function PERMIT_TYPEHASH() external view returns (bytes32);
-
-    function _burnFrom(address account_, uint256 amount_) external;
-
-    function addTWAPSource(address newTWAPSourceDexPool_) external;
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function burn(uint256 amount) external;
-
-    function burnFrom(address account_, uint256 amount_) external;
-
-    function changeTWAPEpochPeriod(uint256 newTWAPEpochPeriod_) external;
-
-    function changeTWAPOracle(address newTWAPOracle_) external;
-
-    function decimals() external view returns (uint8);
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
-
-    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
-
-    function mint(address account_, uint256 amount_) external;
-
-    function name() external view returns (string memory);
-
-    function nonces(address owner) external view returns (uint256);
-
-    function owner() external view returns (address);
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 amount,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
-
-    function removeTWAPSource(address twapSourceToRemove_) external;
-
-    function renounceOwnership() external;
-
-    function setVault(address vault_) external returns (bool);
-
-    function symbol() external view returns (string memory);
-
-    function totalSupply() external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    function transferOwnership(address newOwner_) external;
-
-    function twapEpochPeriod() external view returns (uint256);
-
-    function twapOracle() external view returns (address);
-
-    function vault() external view returns (address);
-}
+import {Burner} from "src/policies/Burner.sol";
+import {Kernel, Actions} from "src/Kernel.sol";
+import {RolesAdmin} from "src/policies/RolesAdmin.sol";
+import {ERC20 as SolmateERC20} from "solmate/tokens/ERC20.sol";
 
 interface OlympusTreasury {
     enum MANAGING {
@@ -338,13 +261,18 @@ contract OwnedERC20 is ERC20, Ownable {
 }
 
 contract MigratorForkTest is Test {
-    OlympusERC20Token public OHMv1;
+    IERC20 public OHMv1;
+    IERC20 public OHMv2;
     OlympusTreasury public treasury;
     OlympusTokenMigrator public migrator;
+    IStaking public staking;
     IgOHM public gOHM;
     OwnedERC20 public antiOHM;
+    Burner public burner;
+    RolesAdmin public rolesAdmin;
 
     address public constant DAO_MS = 0x245cc372C84B3645Bf0Ffe6538620B04a217988B;
+    address public constant TIMELOCK = 0x953EA3223d2dd3c1A91E9D6cca1bf7Af162C9c39;
     uint256 public constant BLOCKS_NEEDED_FOR_QUEUE = 6000;
     uint256 public constant BLOCK_NUMBER = 24070000;
 
@@ -352,13 +280,25 @@ contract MigratorForkTest is Test {
         vm.createSelectFork("mainnet", BLOCK_NUMBER);
 
         // Existing contracts
-        OHMv1 = OlympusERC20Token(0x383518188C0C6d7730D91b2c03a03C837814a899);
+        OHMv1 = IERC20(0x383518188C0C6d7730D91b2c03a03C837814a899);
+        OHMv2 = IERC20(0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5);
         treasury = OlympusTreasury(0x31F8Cc382c9898b273eff4e0b7626a6987C846E8);
         migrator = OlympusTokenMigrator(0x184f3FAd8618a6F458C16bae63F70C426fE784B3);
+        staking = IStaking(0xB63cac384247597756545b500253ff8E607a8020);
         gOHM = IgOHM(0x0ab87046fBb341D058F17CBC4c1133F25a20a52f);
+        rolesAdmin = RolesAdmin(0xb216d714d91eeC4F7120a732c11428857C659eC8);
+
+        Kernel kernel = Kernel(0x2286d7f9639e8158FaD1169e76d1FbC38247f54b);
 
         // Create a dummy token
         antiOHM = new OwnedERC20("AntiOHM", "antiOHM", DAO_MS);
+
+        // Deploy burner
+        burner = new Burner(kernel, SolmateERC20(address(OHMv2)));
+
+        // Install burner into the kernel
+        vm.prank(DAO_MS);
+        kernel.executeAction(Actions.ActivatePolicy, address(burner));
     }
 
     function test_migrate() public {
@@ -370,16 +310,16 @@ contract MigratorForkTest is Test {
         vm.prank(DAO_MS);
         treasury.queue(OlympusTreasury.MANAGING.RESERVETOKEN, address(antiOHM));
 
-        // Confirm that DAO MS is not a reward depositor
+        // Confirm that Timelock is not a reward depositor
         assertFalse(
-            treasury.isReserveDepositor(DAO_MS),
-            "DAO MS should not be a reserve depositor"
+            treasury.isReserveDepositor(TIMELOCK),
+            "Timelock should not be a reserve depositor"
         );
 
-        // Add DAO MS as a reserve depositor
-        console2.log("Adding DAO MS as a reserve depositor...");
+        // Add Timelock as a reserve depositor
+        console2.log("Adding Timelock as a reserve depositor...");
         vm.prank(DAO_MS);
-        treasury.queue(OlympusTreasury.MANAGING.RESERVEDEPOSITOR, DAO_MS);
+        treasury.queue(OlympusTreasury.MANAGING.RESERVEDEPOSITOR, TIMELOCK);
 
         // Warp forward to the timelock expiry
         console2.log("Warpping forward to the timelock expiry...");
@@ -393,13 +333,13 @@ contract MigratorForkTest is Test {
         // Verify that antiOHM is a reserve token
         assertTrue(treasury.isReserveToken(address(antiOHM)), "antiOHM should be a reserve token");
 
-        // Toggle DAO MS as a reserve depositor
-        console2.log("Enabling DAO MS as a reserve depositor...");
+        // Toggle Timelock as a reserve depositor
+        console2.log("Enabling Timelock as a reserve depositor...");
         vm.prank(DAO_MS);
-        treasury.toggle(OlympusTreasury.MANAGING.RESERVEDEPOSITOR, DAO_MS, address(0));
+        treasury.toggle(OlympusTreasury.MANAGING.RESERVEDEPOSITOR, TIMELOCK, address(0));
 
-        // Verify that DAO MS is a reserve depositor
-        assertTrue(treasury.isReserveDepositor(DAO_MS), "DAO MS should be a reserve depositor");
+        // Verify that Timelock is a reserve depositor
+        assertTrue(treasury.isReserveDepositor(TIMELOCK), "Timelock should be a reserve depositor");
 
         console2.log("");
         console2.log("====== Minting OHM v1...");
@@ -438,51 +378,118 @@ contract MigratorForkTest is Test {
         maxOHM += 8245430417;
         uint256 maxAntiOHM = maxOHM * 1e9;
 
-        // Mint antiOHM to the DAO MS
+        // Mint antiOHM to the Timelock
         vm.prank(DAO_MS);
-        antiOHM.mint(DAO_MS, maxAntiOHM);
+        antiOHM.mint(TIMELOCK, maxAntiOHM);
         console2.log("maxAntiOHM (18 dp):", maxAntiOHM);
 
-        // Approve antiOHM for the DAO MS
-        vm.prank(DAO_MS);
+        // The following steps are performed by the Timelock
+        // e.g. through an OCG proposal
+
+        // Grant the timelock the burner_admin role
+        console2.log("Granting the burner_admin role to the Timelock...");
+        vm.prank(TIMELOCK);
+        rolesAdmin.grantRole(bytes32("burner_admin"), TIMELOCK);
+
+        // Add a burner category
+        console2.log("Adding a burner category to the Timelock...");
+        vm.prank(TIMELOCK);
+        burner.addCategory("migration");
+
+        // Approve antiOHM for the Timelock
+        vm.prank(TIMELOCK);
         antiOHM.approve(address(treasury), maxAntiOHM);
 
         // Deposit antiOHM to the treasury
-        vm.prank(DAO_MS);
+        vm.prank(TIMELOCK);
         treasury.deposit(maxAntiOHM, address(antiOHM), 0);
 
-        // Verify that the DAO MS has received OHM
-        console2.log("OHM balance of DAO MS (9 dp):", OHMv1.balanceOf(DAO_MS));
-        assertEq(OHMv1.balanceOf(DAO_MS), maxOHM, "DAO MS should have received 100 OHM");
+        // Verify that the Timelock has received OHM
+        console2.log("OHM balance of Timelock (9 dp):", OHMv1.balanceOf(TIMELOCK));
+        assertEq(OHMv1.balanceOf(TIMELOCK), maxOHM, "Timelock should have received 100 OHM");
 
         console2.log("");
         console2.log("====== Migrating OHM v1 to gOHM...");
 
-        uint256 gOHMBalanceBefore = gOHM.balanceOf(DAO_MS);
-        console2.log("gOHM balance of DAO MS before migration (18 dp):", gOHMBalanceBefore);
+        uint256 gOHMBalanceBefore = gOHM.balanceOf(TIMELOCK);
+        console2.log("gOHM balance of Timelock before migration (18 dp):", gOHMBalanceBefore);
 
         // Approve the migrator to spend the OHM
-        vm.prank(DAO_MS);
+        vm.prank(TIMELOCK);
         OHMv1.approve(address(migrator), maxOHM);
 
         // Migrate the OHM to the migrator
-        vm.prank(DAO_MS);
+        vm.prank(TIMELOCK);
         migrator.migrate(
             maxOHM,
             OlympusTokenMigrator.TYPE.UNSTAKED,
             OlympusTokenMigrator.TYPE.WRAPPED
         );
 
-        uint256 gOHMBalanceDifference = gOHM.balanceOf(DAO_MS) - gOHMBalanceBefore;
-        console2.log("gOHM balance difference (18 dp):", gOHMBalanceDifference);
+        uint256 gOHMBalanceDifference = gOHM.balanceOf(TIMELOCK) - gOHMBalanceBefore;
+        console2.log("gOHM balance difference of Timelock (18 dp):", gOHMBalanceDifference);
 
         // gOHM balance of migrator
         console2.log("gOHM balance of migrator (18 dp):", gOHM.balanceOf(address(migrator)));
 
-        // OHM balance of DAO MS
-        console2.log("OHM balance of DAO MS (9 dp):", OHMv1.balanceOf(DAO_MS));
+        // OHM balance of Timelock
+        console2.log("OHM balance of Timelock (9 dp):", OHMv1.balanceOf(TIMELOCK));
 
-        // antiOHM balance of DAO MS
-        console2.log("antiOHM balance of DAO MS (18 dp):", antiOHM.balanceOf(DAO_MS));
+        // antiOHM balance of Timelock
+        console2.log("antiOHM balance of Timelock (18 dp):", antiOHM.balanceOf(TIMELOCK));
+
+        console2.log("");
+        console2.log("====== Unstaking gOHM to OHM v2...");
+
+        // Approve the staking contract to spend the gOHM
+        vm.prank(TIMELOCK);
+        gOHM.approve(address(staking), gOHMBalanceDifference);
+
+        uint256 OHMv2BalanceBefore = OHMv2.balanceOf(TIMELOCK);
+        uint256 OHMv2TotalSupplyBefore = OHMv2.totalSupply();
+        console2.log("OHM v2 balance of Timelock before unstaking (9 dp):", OHMv2BalanceBefore);
+
+        // Unstake the gOHM
+        vm.prank(TIMELOCK);
+        staking.unstake(TIMELOCK, gOHMBalanceDifference, false, false);
+
+        uint256 OHMv2BalanceDifference = OHMv2.balanceOf(TIMELOCK) - OHMv2BalanceBefore;
+        console2.log("OHM v2 balance difference of Timelock (9 dp):", OHMv2BalanceDifference);
+
+        console2.log("");
+        console2.log("====== Burning OHM v2...");
+
+        // Approve the burner to spend the OHM v2
+        console2.log("Approving the burner to spend the OHM v2...");
+        vm.prank(TIMELOCK);
+        OHMv2.approve(address(burner), OHMv2BalanceDifference);
+
+        // Burn the OHM v2
+        console2.log("Burning the OHM v2...");
+        vm.prank(TIMELOCK);
+        burner.burnFrom(TIMELOCK, OHMv2BalanceDifference, "migration");
+
+        // Verify that the OHM v2 balance of the Timelock is 0
+        console2.log("OHM v2 balance of Timelock (9 dp):", OHMv2.balanceOf(TIMELOCK));
+        assertEq(OHMv2.balanceOf(TIMELOCK), 0, "OHM v2 balance of Timelock should be 0");
+
+        // Verify that the OHM v2 total supply has decreased by the amount of the OHM v2 balance difference
+        console2.log("OHM v2 total supply before unstaking (9 dp):", OHMv2TotalSupplyBefore);
+        console2.log("OHM v2 total supply after unstaking (9 dp):", OHMv2.totalSupply());
+        console2.log(
+            "OHM v2 total supply difference (9 dp):",
+            OHMv2TotalSupplyBefore - OHMv2.totalSupply()
+        );
+        assertEq(
+            OHMv2.totalSupply(),
+            OHMv2TotalSupplyBefore - OHMv2BalanceDifference,
+            "OHM v2 total supply should have decreased by the amount of the OHM v2 balance difference"
+        );
+
+        // Cleanup
+        // Revoke the burner_admin role
+        console2.log("Revoking the burner_admin role from the Timelock...");
+        vm.prank(TIMELOCK);
+        rolesAdmin.revokeRole(bytes32("burner_admin"), TIMELOCK);
     }
 }
