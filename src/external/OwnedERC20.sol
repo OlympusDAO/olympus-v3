8351c33547ddf4cd1a929ace2fdc2fc31c906794
Jem <0x0xjem@gmail.com>
2026-01-19 18:17:09 +0400

    fix: make OwnedERC20 inherit from ERC20Burnable instead of implementing it again

--- src/external/OwnedERC20.sol
+++ src/external/OwnedERC20.sol
@@ -2,10 +2,11 @@
 pragma solidity >=0.8.15;
 
 import {ERC20} from "@openzeppelin-5.3.0/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin-5.3.0/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin-5.3.0/access/Ownable.sol";
 
 /// @notice ERC20 token with owner-only mint, and a burn function
-contract OwnedERC20 is ERC20, Ownable {
+contract OwnedERC20 is ERC20Burnable, Ownable {
     constructor(
         string memory name_,
         string memory symbol_,
@@ -16,17 +17,5 @@ contract OwnedERC20 is ERC20, Ownable {
     /// @dev    Only the owner can mint tokens
     function mint(address to, uint256 amount) public virtual onlyOwner {
         _mint(to, amount);
-    }
-
-    /// @notice Burn tokens from the specified address
-    /// @dev    Caller needs allowance if burning from another address
-    function burn(address from, uint256 amount) public virtual {
-        // If the caller is not the token holder, spend the allowance (or revert)
-        if (from != msg.sender) {
-            _spendAllowance(from, msg.sender, amount);
-        }
-
-        // Burn the tokens
-        _burn(from, amount);
     }
 }
