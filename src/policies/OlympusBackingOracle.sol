bee519add0d28ad4305169aa29b234af81a93261
Yurii3721 <202633286+Yurii3721@users.noreply.github.com>
2026-07-09 15:53:21 +0300

    fix(backing-oracle): change Solidity pragma to `>=0.8.24`

--- src/policies/OlympusBackingOracle.sol
+++ src/policies/OlympusBackingOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.30;
+pragma solidity >=0.8.24;
 
 // Interfaces
 import {IOlympusBackingOracle} from "src/policies/interfaces/IOlympusBackingOracle.sol";
--- src/policies/interfaces/IOlympusBackingOracle.sol
+++ src/policies/interfaces/IOlympusBackingOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.24;
 
 /// @title IOlympusBackingOracle
 /// @notice The interface for the OlympusBackingOracle policy, which serves as the canonical OHM backing value.
